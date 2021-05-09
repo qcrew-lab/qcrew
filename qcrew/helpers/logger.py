d0a7682fe82f4a669646c79d541cff3784da0910
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-09 19:13:56 +0800

    Simplify logger customization

--- qcrew/helpers/logger.py
+++ qcrew/helpers/logger.py
@@ -9,25 +9,10 @@ from loguru import logger
 LOG_FOLDER_NAME = "logs"
 LOG_FOLDER_PATH = Path.cwd() / LOG_FOLDER_NAME
 
-# customize level colors
-logger.level("DEBUG", color="<cyan>")
-logger.level("INFO", color="<white>")
-logger.level("SUCCESS", color="<blue>")
-logger.level("WARNING", color="<magenta>")
-logger.level("ERROR", color="<red>")
-
-# specify record format for logs saved to file and displayed to user
-LOG_FORMAT = (
-    "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level:^7} | {name}:{function}:{line} - {message}"
-)
-
 # register log sink with loguru logger
-# new log file will be created every day at midnight
 logger.add(
     LOG_FOLDER_PATH / "log_{time:YYYYMMDD}.log",
-    rotation="00:00",
-    level="DEBUG",
-    format=LOG_FORMAT,
+    rotation="00:00",  # new log file will be created every day at midnight
 )
 
-logger.info("Logger activated")
+logger.debug("Logger activated")  # log first message

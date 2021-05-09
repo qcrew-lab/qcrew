19e5e6c118b9e9b30f5f3d741a8da0b77d0429f5
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-09 12:27:12 +0800

    Refactor with dataclass, typing hints

--- qcrew/instruments/instrument.py
+++ qcrew/instruments/instrument.py
@@ -1,2 +1,39 @@
-class Instrument():
-    pass
+"""
+This module defines base classes for encapsulating instruments in qcrew's lab.
+"""
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any, NoReturn
+
+
+class Instrument:
+    @property
+    @abstractmethod
+    def parameters(self) -> dict[str, Any]:
+        pass
+
+    @property
+    @abstractmethod
+    def status(self) -> dict[str, bool]:
+        pass
+
+
+@dataclass
+class PhysicalInstrument(Instrument):
+    uid: Any
+
+    def __post_init__(self):
+        self._connect()
+        self._initialize()
+
+    @abstractmethod
+    def _connect(self) -> Any:
+        pass
+
+    @abstractmethod
+    def _initialize(self) -> NoReturn:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> NoReturn:
+        pass

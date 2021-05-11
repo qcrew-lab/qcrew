640d700ae2a8f288e243108f1abfea423e55c1db
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-11 19:19:09 +0800

    Feat deterministic parameter dict generation

--- qcrew/instruments/instrument.py
+++ qcrew/instruments/instrument.py
@@ -3,42 +3,42 @@ This module defines abstract base classes for encapsulating instruments in qcrew
 TODO WRITE DOCUMENTATION
 """
 from abc import abstractmethod
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 from typing import Any, NoReturn
 
 from qcrew.helpers import Yamlable
 
-STATUS_DICT = {"staged": False}
+STATUS_DICT = {"staged": False, "connected": False, "running": False}  # explain these
 
 
 @dataclass
 class Instrument(Yamlable):
-    status: dict[str, bool] = field(
-        default_factory=STATUS_DICT.copy, init=False, compare=False
-    )
+    _status: dict[str, Any] = field(default_factory=STATUS_DICT.copy, init=False)
+    _parameters: dict[str, Any] = field(default_factory=dict, init=False)
 
     @property
-    @abstractmethod
+    def status(self) -> dict[str, Any]:
+        """ """
+        # condition - self._status must represent current status
+        return self._status
+
+    @property
     def parameters(self) -> dict[str, Any]:
         """ """
+        # condition - every key in self._parameters must be an instance attribute
+        return {k: getattr(self, k) for k in self._parameters}
 
 
 @dataclass
 class PhysicalInstrument(Instrument):
-    id: Any  # OK to shadow built-in `id` as `PhysicalInstrument` ids ought to be unique
-    _handle: Any = field(default=None, init=False, repr=False, compare=False)
-
-    def __post_init__(self):
-        self.status.update({"connected": False, "running": False})
-        self._connect()
-        self._initialize()
+    id: Any  # OK to shadow built-in `id` as instrument ids ought to be unique
 
     @abstractmethod
     def _connect(self) -> NoReturn:
         """ """
 
     @abstractmethod
-    def _initialize(self) -> NoReturn:
+    def _initialize(self, *initial_parameters) -> NoReturn:
         """ """
 
     @abstractmethod

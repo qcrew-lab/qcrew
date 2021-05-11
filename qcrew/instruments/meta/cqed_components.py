bf6b15b639b2a3eb6c414c1b48e83efee79a6100
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-11 22:28:08 +0800

    Define IQMixer dataclass

--- qcrew/instruments/meta/cqed_components.py
+++ qcrew/instruments/meta/cqed_components.py
@@ -1,39 +1,50 @@
-from dataclasses import dataclass
-from typing import Any
+""" """
+from dataclasses import dataclass, field
+from typing import Any, ClassVar, Union
 
-from qcrew.instruments import Instrument
+from qcrew.instruments import Instrument, LabBrick
 
 
 @dataclass
-class QuantumElement(Instrument):
+class IQMixer:
     """ """
 
-    # _lo
-    lo_freq: float
-    # int_freq
-    # ports
-    # mixer - name, offsets {i, q, g, p}
-    # operations
+    # class variable defining the dictionary of offsets for IQMixer objects
+    iqmixer_offsets_dict: ClassVar[dict[str, float]] = {
+        "i": None,  # DC offset applied by a OPX AO port to the I port of the IQMixer
+        "q": None,  # DC offset applied by a OPX AO port to the Q port of the IQMixer
+        "g": None,  # offset used by OPX to correct the gain imbalance of the IQMixer
+        "p": None,  # offset used by OPX to correct the phase imbalance of the IQMixer
+    }
+
+    name: str = field(default=None, init=False)
+    offsets: dict[str, float] = field(  # to store offsets of this IQMixer instance
+        default_factory=iqmixer_offsets_dict.copy, init=False
+    )
+
+
+@dataclass
+class Qubit(Instrument):
+    """ """
+
+    name: str
+    lo: LabBrick
+    mixer: IQMixer
+    int_freq: Union[int, float]
+    # ports named tuple
+    # operations is a dict of str keys and pulse values
+
+
+@dataclass
+class ReadoutResonator(Instrument):
+    """ """
+    # qubit params AND tof, smearing, ro-related-params
+
+@dataclass
+class StorageCavity(Instrument):
+    """ """
 
 
 @dataclass
 class QuantumDevice(Instrument):
     """ """
-
-
-@dataclass
-class Qubit(QuantumElement):
-    """ """
-
-    # ports override
-
-    @property
-    def parameters(self) -> dict[str, Any]:
-        return {"lo_freq": self.lo_freq}
-
-
-@dataclass
-class ReadoutResonator(QuantumElement):
-    """ """
-
-    # ports override

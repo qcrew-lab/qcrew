dc5429427ce53fb1d9df5469c49253d6bc63d4d9
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-12 11:27:38 +0800

    Separate class and instance attributes

--- qcrew/instruments/instrument.py
+++ qcrew/instruments/instrument.py
@@ -13,35 +13,43 @@ from qcrew.helpers import Yamlable
 class Instrument(Yamlable):
     """ """
 
-    # class variable defining the dictionary of statuses for Instrument objects
-    instrument_status_dict: ClassVar[dict[str, bool]] = {
-        "staged": False,
-        "connected": False,
-        "running": False,
-    }
+    # class variable defining the default status dict for Instrument objects
+    _status_dict: ClassVar[dict[str, bool]] = {"staged": False}
 
-    _status: dict[str, Any] = field(  # to store statuses of this Instrument instance
-        default_factory=instrument_status_dict.copy, init=False
+    # subclasses to override these dictionaries as they deem fit
+    _status: dict[str, Any] = field(
+        default_factory=_status_dict.copy, init=False, repr=False
     )
-
-    _parameters: dict[str, Any] = field(default_factory=dict, init=False)
+    _parameters: dict[str, Any] = field(default_factory=dict, init=False, repr=False)
 
     @property
     def status(self) -> dict[str, Any]:
         """ """
         # condition - self._status must represent current status
+        # subclasses are responsible for tracking current status
         return self._status
 
     @property
     def parameters(self) -> dict[str, Any]:
         """ """
         # condition - every key in self._parameters must be an instance attribute
+        # subclasses are responsible for defining the attribute getter
         return {k: getattr(self, k) for k in self._parameters}
 
 
 @dataclass
 class PhysicalInstrument(Instrument):
     """ """
+
+    # class variable defining the default status dict for PhysicalInstrument objects
+    _status_dict: ClassVar[dict[str, bool]] = {
+        "staged": False,
+        "connected": False,
+        "running": False,
+    }
+
+    # stores statuses of this instance
+    _status: dict[str, Any] = field(default_factory=_status_dict.copy, init=False)
 
     id: Any  # OK to shadow built-in `id` as instrument ids ought to be unique
 

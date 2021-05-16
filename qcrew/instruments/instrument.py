8fe4dfdce3c701d2881499447b90d4b650c0be60
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-16 12:00:01 +0800

    Remove status tracking

--- qcrew/instruments/instrument.py
+++ qcrew/instruments/instrument.py
@@ -3,7 +3,7 @@ This module defines abstract base classes for encapsulating instruments in qcrew
 TODO WRITE DOCUMENTATION
 """
 from abc import abstractmethod
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Any, ClassVar, NoReturn
 
 from qcrew.helpers.yamlizer import Yamlable
@@ -13,24 +13,8 @@ from qcrew.helpers.yamlizer import Yamlable
 class Instrument(Yamlable):
     """ """
 
-    # subclasses to override these _status_dict and _parameters as they deem fit
-
-    # class variable defining the status keys for Instrument objects
-    _status_dict: ClassVar[dict[str, bool]] = dict()
     # class variable defining the default parameter set for Instrument objects
-    _parameters: ClassVar[frozenset[str]] = frozenset()
-
-    # stores statuses of this Instrument instance
-    _status: dict[str, bool] = field(
-        default_factory=_status_dict.copy, init=False, repr=False
-    )
-
-    @property
-    def status(self) -> dict[str, Any]:
-        """ """
-        # condition - self._status must represent current status
-        # subclasses are responsible for tracking current status
-        return self._status
+    _parameters: ClassVar[frozenset[str]] = frozenset()  # subclasses to override
 
     @property
     def parameters(self) -> dict[str, Any]:
@@ -45,16 +29,6 @@ class Instrument(Yamlable):
 @dataclass
 class PhysicalInstrument(Instrument):
     """ """
-
-    # class variable defining the status keys for PhysicalInstrument objects
-    _status_dict: ClassVar[dict[str, bool]] = {
-        "staged": False,
-        "connected": False,
-        "running": False,
-    }
-
-    # stores statuses of this instance
-    _status: dict[str, bool] = field(default_factory=_status_dict.copy, init=False)
 
     id: Any  # OK to shadow built-in `id` as instrument ids ought to be unique
 

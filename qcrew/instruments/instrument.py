2025f5024827d1447509dc39f44b09331aeba067
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-20 16:48:12 +0800

    Get parameters recursively

--- qcrew/instruments/instrument.py
+++ qcrew/instruments/instrument.py
@@ -5,6 +5,7 @@ TODO WRITE DOCUMENTATION
 from abc import abstractmethod
 from typing import Any, ClassVar, NoReturn
 
+from qcrew.helpers import logger
 from qcrew.helpers.yamlizer import Yamlable
 
 
@@ -12,14 +13,25 @@ class Instrument(Yamlable):
     """ """
 
     # class variable defining the default parameter set for Instrument objects
-    _parameters: ClassVar[frozenset] = frozenset()  # subclasses to override
+    _parameters: ClassVar[set] = set()  # subclasses to override
 
     @property
     def parameters(self) -> dict[str, Any]:
         """ """
-        # condition - every key in self._parameters must be an instance attribute
-        # subclasses are responsible for defining the attribute getter
-        return {param: getattr(self, param) for param in sorted(list(self._parameters))}
+        param_list = sorted(list(self._parameters))
+        param_dict = dict()
+        for param in param_list:
+            try:
+                param_value = getattr(self, param)
+            except AttributeError as e:
+                logger.exception(f"Instrument parameter '{param}' is not its attribute")
+                raise SystemExit("Failed to get parameters, exiting...") from e
+            else:
+                if isinstance(param_value, Instrument):
+                    param_dict[param] = param_value.parameters  # get params recursively
+                else:
+                    param_dict[param] = param_value
+        return param_dict
 
     # TODO parameters.setter
 

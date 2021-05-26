337e48bc24cc13a7515a6d342da081cae5a49164
Atharv Joshi <atharvjoshi@ymail.com>
2021-05-26 13:36:44 +0800

    Add equality methods for Waveform and Pulse

--- qcrew/helpers/pulsemaker.py
+++ qcrew/helpers/pulsemaker.py
@@ -1,7 +1,7 @@
 """ """
 
 from numbers import Number
-from typing import Callable, ClassVar, NoReturn, Union
+from typing import Callable, ClassVar, Union
 
 import numpy as np
 from qcrew.analysis.funclib import constant_fn, gaussian_fn
@@ -24,6 +24,11 @@ class Waveform(Yamlable):
         """ """
         return f"{type(self).__name__}[{self.fn}, {self.args}]"
 
+    def __eq__(self, other: object):
+        if isinstance(other, Waveform):
+            return self.fn == other.fn and self.args == other.args
+        return False
+
     @property  # function name getter
     def fn(self) -> str:
         return self._fn
@@ -35,16 +40,13 @@ class Waveform(Yamlable):
     @property  # samples getter
     def samples(self) -> Union[Number, np.ndarray]:
         try:
-            samples = func_map[self.fn](*self.args)
+            return func_map[self.fn](*self.args)
         except KeyError as ke:
             logger.exception(f"Unrecognized function '{self.fn}'")
             raise SystemExit("Failed to get waveform samples, exiting...") from ke
         except TypeError as te:
             logger.exception(f"Invalid arguments = {self.args} passed to {self.fn} fn")
             raise SystemExit("Failed to get waveform samples, exiting...") from te
-        else:
-            logger.success(f"Got {len(samples)} sample(s) of {self}")
-            return samples
 
 
 class ControlPulse(Yamlable):
@@ -55,19 +57,26 @@ class ControlPulse(Yamlable):
         [
             frozenset(["single"]),
             frozenset(["I", "Q"]),
+            frozenset(["I"]),
+            frozenset(["Q"]),
         ]
     )
 
     def __init__(self, length: int, waveforms: dict[str, Waveform]) -> None:
         """ """
+        logger.info(f"Making {type(self).__name__}...")
         self.length: int = length
         self._waveforms: dict[str, Waveform] = dict()  # to be updated by setter
         self.waveforms = waveforms
-        logger.info(f"Created {self}")
 
     def __repr__(self) -> str:
         """ """
         return f"{type(self).__name__}[len: {self.length}, {self.waveforms}]"
+
+    def __eq__(self, other: object):
+        if isinstance(other, ControlPulse):
+            return self.length == other.length and self.waveforms == other.waveforms
+        return False
 
     @property  # waveforms getter
     def waveforms(self) -> dict[str, Waveform]:
@@ -75,7 +84,7 @@ class ControlPulse(Yamlable):
         return self._waveforms
 
     @waveforms.setter
-    def waveforms(self, new_wfs: dict[str, Waveform]) -> NoReturn:
+    def waveforms(self, new_wfs: dict[str, Waveform]) -> None:
         """ """
         valid_keysets = [set(keyset) for keyset in self._wfs_keysets]
         try:
@@ -85,16 +94,12 @@ class ControlPulse(Yamlable):
                         logger.warning(f"Invalid {waveform = }, must be of {Waveform}")
                     else:
                         self._waveforms[key] = waveform
-                        logger.success(f"Set '{key}' to {waveform}")
             else:
                 logger.warning(f"Invalid keys found, must be one of {valid_keysets = }")
         except (TypeError, AttributeError):
             logger.exception(f"Expect {dict[str, Waveform]} with {valid_keysets = }")
-
-    @property  # has_single_waveform getter
-    def has_single_waveform(self) -> bool:
-        """ """
-        return set(self._waveforms) == {"single"}
+        else:
+            logger.success(f"Made {self}")
 
 
 IWType = dict[str, tuple[Callable[int, np.ndarray], Callable[int, np.ndarray]]]
@@ -114,9 +119,10 @@ class MeasurementPulse(ControlPulse):
         self.iw: IWType = iw
         super().__init__(**kwargs)
 
-    @property  # integration weights names list getter
-    def iw_names(self) -> list[str]:
-        return [*self.iw]
+    def __eq__(self, other: object):
+        if isinstance(other, MeasurementPulse):
+            return self.length == other.length and self.waveforms == other.waveforms
+        return False
 
 
 Pulse = (ControlPulse, MeasurementPulse)  # for isinstance checks
@@ -132,14 +138,14 @@ DEFAULT_CONSTANT_WAVEFORM = Waveform(fn="constant", args=(0.32))
 DEFAULT_GAUSSIAN_WAVEFORM = Waveform(fn="gaussian", args=(0.25, 1000, 4))
 
 DEFAULT_CW_PULSE = ControlPulse(
-    length=1000, waveforms={"I": DEFAULT_CONSTANT_WAVEFORM, "Q": ZERO_WAVEFORM}
+    length=10000, waveforms={"I": DEFAULT_CONSTANT_WAVEFORM, "Q": ZERO_WAVEFORM}
 )
 DEFAULT_GAUSSIAN_PULSE = ControlPulse(
     length=4000,
     waveforms={"I": DEFAULT_GAUSSIAN_WAVEFORM, "Q": ZERO_WAVEFORM},
 )
 DEFAULT_READOUT_PULSE = MeasurementPulse(
-    length=1000, waveforms={"I": DEFAULT_CONSTANT_WAVEFORM, "Q": ZERO_WAVEFORM}
+    length=800, waveforms={"I": DEFAULT_CONSTANT_WAVEFORM, "Q": ZERO_WAVEFORM}
 )
 
 logger.enable(__name__)  # resume logging

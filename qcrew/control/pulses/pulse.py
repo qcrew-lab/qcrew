5f97e4063d7ebaf78eef69d756404ebedc683c5a
Atharv Joshi <atharvjoshi@ymail.com>
2021-06-01 18:32:00 +0800

    Implement pulse and waveform objects in standalone module

--- qcrew/control/pulses/pulse.py
+++ qcrew/control/pulses/pulse.py
@@ -1,8 +1,9 @@
 """ """
 
 from numbers import Number
-from typing import ClassVar
+from typing import Callable, ClassVar
 
+import numpy as np
 from qcrew.control.pulses.waveform import Waveform
 from qcrew.helpers import logger
 from qcrew.helpers.parametrizer import Paramable
@@ -11,36 +12,173 @@ from qcrew.helpers.parametrizer import Paramable
 class Pulse(Paramable):
     """ """
 
-    # class variable defining the parameter set for Pulse objects
-    _parameters: ClassVar[set[str]] = set(["pulse_type", "length" "waveforms"])
+    _parameters: ClassVar[set[str]] = {"pulse_type", "length", "waveforms"}
 
-    # pylint: disable=invalid-name
-    # "I" and "Q" have specific meanings that are well understood by qcrew
+    _keyset: ClassVar[tuple[str]] = ("I", "Q", "single")
+    _keysets: ClassVar[tuple[tuple[str, ...]]] = (("I", "Q"), ("single"))
 
     def __init__(
-        self,
-        length: int = 400,
-        *,
-        I: tuple[str, Number, ...] = None,
-        Q: tuple[str, Number, ...] = None,
-        single: tuple[str, Number, ...] = None
+        self, length: int = None, **waveforms: tuple[str, Number, ...]
     ) -> None:
         """ """
+        logger.info(f"Creating {type(self).__name__}...")
         self._length: int = length
-        self._waveforms: dict[str, Waveform] = dict()
+        self._waveforms: dict[str, Waveform] = dict()  # updated by setter
+        if waveforms:
+            self.waveforms = waveforms
+
+    def __repr__(self) -> str:
+        """ """
+        return f"{type(self).__name__}[len: {self.length}, {self.waveforms}]"
 
     @property  # pulse type getter
     def pulse_type(self) -> str:
         """ """
         return "control"
 
+    @property  # pulse length getter
+    def length(self) -> int:
+        """ """
+        has_mix_wfs, has_const_wfs = self.has_mix_waveforms, self.has_constant_waveforms
+        if has_mix_wfs and not has_const_wfs:
+            i_wf_len = len(self._waveforms["I"].samples)
+            q_wf_len = len(self._waveforms["Q"].samples)
+            if i_wf_len != q_wf_len:  # must be equal for pulse to be valid
+                logger.error(f"Waveforms have unequal lengths {i_wf_len}, {q_wf_len}")
+            return i_wf_len
+        elif has_const_wfs:
+            if self._length is None:
+                logger.error("Pulse length not defined")
+                raise SystemExit("Failed to get pulse length, exiting...")
+            return self._length
+        else:
+            try:
+                return len(self._waveforms["single"].samples)
+            except KeyError as e:
+                logger.exception(f"Invalid waveform keys, must be in {self._keysets}")
+                raise SystemExit("Failed to get pulse length, exiting...") from e
+
+    @length.setter
+    def length(self, new_length: int) -> None:
+        """ """
+        self._length = new_length
+
+    @property  # waveforms getter
+    def waveforms(self) -> dict[str, Waveform]:
+        """ """
+        valid_keysets = self._keysets
+        keyset = (*self._waveforms,)
+        if keyset not in valid_keysets:
+            logger.error(f"Invalid waveform {keyset = }, {valid_keysets = }")
+            raise SystemExit("Failed to get pulse waveforms, exiting...")
+        else:
+            return self._waveforms
+
+    @waveforms.setter
+    def waveforms(self, new_waveforms: dict[str, tuple[str, Number, ...]]) -> None:
+        """ """
+        try:
+            valid_keys = self._keyset
+            for key in valid_keys:
+                if key in new_waveforms and hasattr(self, key):
+                    setattr(self, key, new_waveforms["I"])
+                else:
+                    logger.warning(f"Ignored invalid {key = }, {valid_keys = }")
+        except TypeError as e:
+            logger.error(f"Expect {dict[str, tuple]}")
+            raise SystemExit("Failed to set pulse waveforms, exiting...") from e
+
+    def _get_waveform(self, key: str) -> Waveform:
+        """ """
+        try:
+            return self._waveforms[key]
+        except KeyError:
+            logger.error(f"No '{key}' waveform defined for this pulse")
+
+    def _set_waveform(self, key: str, new_waveform: tuple[str, Number, ...]) -> None:
+        """ """
+        if key in self._waveforms:  # update existing waveform
+            waveform = self._waveforms[key]
+            waveform.fn, waveform.args = new_waveform[0], new_waveform[1:]
+            logger.success(f"Updated '{key}' to {waveform}")
+        else:  # create new waveform
+            waveform = Waveform(*new_waveform)
+            self._waveforms[key] = waveform
+            logger.success(f"Set '{key}' to {waveform}")
+
+    # pylint: disable=invalid-name
+    # "I" and "Q" have specific meanings that are well understood by qcrew
+
+    @property  # I waveform getter
+    def I(self) -> Waveform:
+        """ """
+        return self._get_waveform("I")
+
+    @I.setter
+    def I(self, new_waveform: tuple[str, Number, ...]) -> None:
+        """ """
+        self._set_waveform("I", new_waveform)
+
+    @property  # Q waveform getter
+    def Q(self) -> Waveform:
+        """ """
+        return self._get_waveform("Q")
+
+    @Q.setter
+    def Q(self, new_waveform: tuple[str, Number, ...]) -> None:
+        """ """
+        self._set_waveform("Q", new_waveform)
+
+    # pylint: enable=invalid-name
+
+    @property  # single waveform getter
+    def single(self) -> Waveform:
+        """ """
+        return self._get_waveform("single")
+
+    @single.setter
+    def single(self, new_waveform: tuple[str, Number, ...]) -> None:
+        """ """
+        self._set_waveform("single", new_waveform)
+
+    @property  # has_mix_waveforms getter
+    def has_mix_waveforms(self) -> bool:
+        """ """
+        return set(self._waveforms) == {"I", "Q"}
+
+    @property  # has_constant_waveforms getter
+    def has_constant_waveforms(self) -> bool:
+        """ """
+        for waveform in self._waveforms:
+            if not waveform.is_constant:
+                return False
+        return True
+
 
 class ReadoutPulse(Pulse):
     """ """
 
-    # pylint: enable=invalid-name
+    # iw means integration weights
+    iw_type = dict[str, tuple[Callable[int, np.ndarray], Callable[int, np.ndarray]]]
+
+    _default_iw: ClassVar[iw_type] = {  # TODO remove defaults once we are ready
+        "iw1": (np.ones, np.zeros),  # iw_name: (cos_fn, sin_fn)
+        "iw2": (np.zeros, np.ones),
+    }
 
     @property  # pulse type getter
     def pulse_type(self) -> str:
         """ """
         return "measurement"
+
+    @property  # integration weights getter
+    def iw(self) -> None:
+        """ """
+        return self._default_iw.copy()
+
+    @property  # integration weight names
+    def iw_names(self) -> list[str]:
+        return [*self._default_iw]
+
+p = Pulse(length=400)
+print(p.parameters)
--- qcrew/control/pulses/waveform.py
+++ qcrew/control/pulses/waveform.py
@@ -1,47 +1,42 @@
 """ """
 
 from numbers import Number
-from typing import ClassVar
+from typing import ClassVar, Union
 
 from qcrew.analyze.funclib import constant_fn, gaussian_fn
 from qcrew.helpers import logger
 from qcrew.helpers.parametrizer import Paramable
 
-func_map = {"constant": constant_fn, "gaussian": gaussian_fn}
+_FUNC_MAP = {"constant": constant_fn, "gaussian": gaussian_fn}
+
 
 class Waveform(Paramable):
     """ """
 
-    # class variable defining the parameter set for Pulse objects
-    _parameters: ClassVar[set[str]] = set(["fn", "args"])
+    _parameters: ClassVar[set[str]] = {"fn", "args"}
 
-    def __init__(self, fn: str, args: tuple[Number]) -> None:
+    def __init__(self, fn: str, *args: Number) -> None:
         """ """
-        self._fn: str = str(fn)  # the key in func_map of the waveform's function
-        self._args: tuple[Number] = args  # arguments passed to the waveform's function
+        self.fn: str = str(fn)  # key in func_map of the waveform's fn
+        self.args: tuple[Number] = args  # args passed to waveform's fn
 
     def __repr__(self) -> str:
         """ """
-        return f"{type(self).__name__}[{self._fn}, {self._args}]"
+        return f"{type(self).__name__}[{self.fn}, {self.args}]"
 
-    @property  # fn name getter
-    def fn(self) -> str:
+    @property  # is_constant getter
+    def is_constant(self) -> bool:
         """ """
-        return self._fn
-
-    @property  # args getter
-    def args(self) -> tuple[Number]:
-        """ """
-        return self._args
+        return self.fn == "constant"
 
     @property  # samples getter
-    def samples(self) -> Union[Number, np.ndarray]:
+    def samples(self) -> Union[Number, list]:
         """ """
         try:
-            return func_map[self.fn](*self.args)
+            return _FUNC_MAP[self.fn](*self.args)
         except KeyError as ke:
             logger.exception(f"Unrecognized function '{self.fn}'")
             raise SystemExit("Failed to get waveform samples, exiting...") from ke
         except TypeError as te:
-            logger.exception(f"Invalid arguments = {self.args} passed to {self.fn} fn")
+            logger.exception(f"Invalid args = {self.args} passed to {self.fn} fn")
             raise SystemExit("Failed to get waveform samples, exiting...") from te

a00097888705566782ab209b2a0d3bb24c1de605
Atharv Joshi <atharvjoshi@ymail.com>
2021-06-06 23:26:59 +0800

    Redesign based on changes in "mode" module

---
+++ qcrew/control/modes/qubit.py
@@ -0,0 +1,9 @@
+""" """
+
+from qcrew.control.modes.mode import Mode
+
+
+class Qubit(Mode):
+    """ """
+    pass
+    # TODO here we will add "selective" pulses and methods such as rotate()
---
+++ qcrew/control/modes/readout_resonator.py
@@ -0,0 +1,9 @@
+""" """
+
+from qcrew.control.modes.mode import ReadoutMode
+
+
+class ReadoutResonator(ReadoutMode):
+    """ """
+    pass
+    # TODO here we will add "selective" pulses and methods such as measure()

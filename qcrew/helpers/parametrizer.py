0e54463734135c256421b6c94914b2f6009a353e
Atharv Joshi <atharvjoshi@ymail.com>
2021-06-06 22:02:22 +0800

    Rename 'Paramable' to 'Parametrized'
    Add parameters setter v0

--- qcrew/helpers/parametrizer.py
+++ qcrew/helpers/parametrizer.py
@@ -5,10 +5,10 @@ from typing import Any, ClassVar
 from qcrew.helpers import logger
 from qcrew.helpers.yamlizer import Yamlable
 
-class Paramable(Yamlable):
+class Parametrized(Yamlable):
     """ """
 
-    # class variable defining the default parameter set for Paramable objects
+    # class variable defining the default parameter set for Parametrized objects
     _parameters: ClassVar[set[str]] = set()  # subclasses to override
 
     @property
@@ -24,10 +24,16 @@ class Paramable(Yamlable):
                 logger.exception(f"Parameter '{param}' must be an attribute of {cls_}")
                 raise SystemExit("Failed to get parameters, exiting...") from e
             else:
-                if isinstance(param_value, Paramable):
+                if isinstance(param_value, Parametrized):
                     param_dict[param] = param_value.parameters  # get params recursively
                 else:
                     param_dict[param] = param_value
         return param_dict
 
-    # TODO parameters.setter
+    @parameters.setter
+    def parameters(self, new_parameters: dict[str, Any]) -> None:
+        """ """
+        # TODO error handling and logging
+        for param, value in new_parameters.items():
+            if param in self._parameters:
+                setattr(self, param, value)

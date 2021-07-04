9d264c40b69031e556fc170202118aacc84ac98b
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-04 11:28:48 +0800

    Add hard-coded constant integration weights (temporary)

--- qcrew/control/pulses/pulses.py
+++ qcrew/control/pulses/pulses.py
@@ -1,14 +1,16 @@
 """ """
 
 from numbers import Real
-from typing import Any, ClassVar
+from typing import ClassVar
 
 import numpy as np
+from qcrew.helpers import logger
 from qcrew.helpers.parametrizer import Parametrized
-from qcrew.control.instruments.quantum_machines import BASE_AMP
+from qcrew.control.instruments.quantum_machines import BASE_AMP, CLOCK_CYCLE
 
 
 class Pulse(Parametrized):
+    """ """
 
     _parameters: ClassVar[set[str]] = {"length"}
 
@@ -16,16 +18,18 @@ class Pulse(Parametrized):
         self,
         length: int,
         has_mix_waveforms: bool = True,
-        integration_weights: dict[str, np.ndarray] = None,
+        integration_weights: dict[str, dict[str, np.ndarray]] = None,
     ) -> None:
         """ """
         self.length: int = length
         self.has_mix_waveforms: bool = has_mix_waveforms
         self.is_readout_pulse: bool = False
+        self._integration_weights: dict[str, dict[str, np.ndarray]] = None
+
         if integration_weights is not None:
+            self._integration_weights = integration_weights
             self.is_readout_pulse = True
             self._parameters.add("integration_weights")
-            self.integration_weights: dict[str, np.ndarray] = integration_weights
 
     def __repr__(self) -> str:
         """ """
@@ -42,6 +46,26 @@ class Pulse(Parametrized):
         """ """
         raise NotImplementedError  # subclasses must implements
 
+    @property  # integration weights getter (NOTE: hard coded, for now)
+    def integration_weights(self) -> dict[str, dict[str, np.ndarray]]:
+        """ """
+        if self._integration_weights is None:
+            logger.warning(f"Integration weights not defined for {self}")
+        elif not self._integration_weights:  # empty dict, return default
+            iw_length = int(self.length / CLOCK_CYCLE)
+            return {
+                "iw1": {
+                    "cosine": np.ones(iw_length),
+                    "sine": np.zeros(iw_length),
+                },
+                "iw2": {
+                    "cosine": np.zeros(iw_length),
+                    "sine": np.ones(iw_length),
+                },
+            }
+        else:
+            return self._integration_weights
+
     @property  # pulse type_ getter for building QM config
     def type_(self) -> str:
         """ """
@@ -56,8 +80,8 @@ class ConstantPulse(Pulse):
     def __init__(
         self,
         ampx: float = 1.0,
-        length: int = 400,
-        integration_weights: dict[str, np.ndarray] = None,
+        length: int = 1000,
+        integration_weights: dict[str, Pulse] = None,
     ) -> None:
         """ """
         self.ampx: float = ampx
@@ -65,12 +89,12 @@ class ConstantPulse(Pulse):
 
     @property
     def samples(self) -> tuple[np.ndarray]:
-        i_wave = np.full(self.length, (BASE_AMP * self.ampx))
-        q_wave = np.zeros(self.length)
-        return i_wave, q_wave
+        return np.full(self.length, (BASE_AMP * self.ampx)), np.zeros(self.length)
 
 
 class GaussianPulse(Pulse):
+    """ """
+
     _parameters: ClassVar[set[str]] = Pulse._parameters | {
         "sigma",
         "chop",
@@ -84,7 +108,7 @@ class GaussianPulse(Pulse):
         chop: int = 6,
         ampx: float = 1.0,
         drag: float = 0.0,
-        integration_weights: dict[str, np.ndarray] = None,
+        integration_weights: dict[str, Pulse] = None,
     ) -> None:
         """ """
         self.sigma: float = sigma
@@ -96,11 +120,12 @@ class GaussianPulse(Pulse):
 
     def __call__(self, sigma: float, chop: int, ampx: float, drag: float) -> None:
         """ """
-        super().__call__(sigma=sigma, chop=chop, ampx=ampx, drag=drag)
-        self.length = int(sigma * chop)
+        length = int(sigma * chop)
+        super().__call__(sigma=sigma, chop=chop, ampx=ampx, drag=drag, length=length)
 
     @property
     def samples(self) -> tuple[np.ndarray]:
+        """ """
         start, stop = -self.chop / 2 * self.sigma, self.chop / 2 * self.sigma
         ts = np.linspace(start, stop, self.length)
         i_wave = BASE_AMP * self.ampx * np.exp(-(ts ** 2) / (2.0 * self.sigma ** 2))

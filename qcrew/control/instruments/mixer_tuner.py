d9fc7dfe62fc38f40ae3e74aeb3893d3ad5046b8
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-10 13:13:25 +0800

    Follow DRY
    Add maxiter as class attribute and reduce threshold to 1
    
    make obj fn return abs contrast

--- qcrew/control/instruments/mixer_tuner.py
+++ qcrew/control/instruments/mixer_tuner.py
@@ -10,8 +10,8 @@ from qcrew.control.instruments.quantum_machines.qm_config_builder import QMConfi
 from qcrew.control.instruments.signal_hound.sa124 import Sa124
 from qcrew.control.modes.mode import Mode
 from qcrew.helpers import logger
-from qm.QmJob import QmJob
 from qm import _Program
+from qm.QmJob import QmJob
 from qm.qua import infinite_loop_, program
 from qm.QuantumMachine import QuantumMachine
 
@@ -20,7 +20,8 @@ class MixerTuner:
     """ """
 
     simplex: np.ndarray = np.array([[0.0, 0.0], [0.0, 0.1], [0.1, 0.0]])
-    threshold: float = 3.0  # in dBm
+    threshold: float = 1.0  # in dBm
+    maxiter: int = 100
     span: float = 2e6
     rbw: float = 50e3
     ref_power: float = 0.0
@@ -35,13 +36,6 @@ class MixerTuner:
         self._modes: tuple[Mode] = modes
         logger.info(f"Call `.tune()` to tune mixers of {self._modes}")
 
-    def _get_qua_program(self, mode: Mode) -> _Program:
-        """ """
-        with program() as mixer_tuning:
-            with infinite_loop_():
-                mode.play("constant_pulse")
-        return mixer_tuning
-
     def tune(self) -> None:
         """ """
         try:
@@ -53,7 +47,8 @@ class MixerTuner:
             logger.error("MixerTuner is initialized with unrecognized arguments")
             raise SystemExit("Failed to initiate mixer tuning, exiting...") from e
         else:
-            self._sa.rbw = Sa124.default_rbw
+            self._sa.rbw = Sa124.default_rbw  # restore sa and qm states
+            self._sa.ref_power = Sa124.default_ref_power
             job.halt()
 
     def _tune_mode(self, mode: Mode, job: QmJob) -> None:
@@ -63,49 +58,41 @@ class MixerTuner:
             is_tuned, center_idx, floor = self._check_tuning(center=center)
             if is_tuned:
                 logger.success(f"{key} already tuned to within {self.threshold}dBm!")
-            elif key == "LO":
-                self._tune_lo(mode, center_idx, floor)
+                return
+            logger.info(f"Minimizing {mode} {key} leakage...")
+            if key == "LO":
+                i_offset, q_offset = self._tune_lo(mode, center_idx, floor)
+                if i_offset is not None and q_offset is not None:
+                    mode.offsets = {"I": i_offset, "Q": q_offset}
             elif key == "SB":
-                self._tune_sb(mode, job, center_idx, floor)
+                g_offset, p_offset = self._tune_sb(mode, job, center_idx, floor)
+                if g_offset is not None and p_offset is not None:
+                    mode.offsets = {"G": g_offset, "P": p_offset}
 
-    def _tune_lo(self, mode: Mode, center_idx: int, floor: float) -> None:
+    def _tune_lo(self, mode: Mode, center_idx: int, floor: float) -> tuple[float]:
         """ """
-        logger.info(f"Minimizing {mode} local oscillator leakage...")
 
         def objective_fn(offsets: tuple[float]) -> float:
             i_offset, q_offset, mode_name = offsets[0], offsets[1], mode.name
             self._qm.set_output_dc_offset_by_element(mode_name, "I", i_offset)
             self._qm.set_output_dc_offset_by_element(mode_name, "Q", q_offset)
-            _, amps = self._sa.sweep()
-            contrast = amps[center_idx] - floor
+            contrast = self._get_contrast(center_idx, floor)
             return contrast
 
-        start_time = time.perf_counter()
         result = self._minimize(objective_fn)
-        if result is not None:
-            time_ = time.perf_counter() - start_time
-            contrast = objective_fn(result)
-            logger.success(f"LO leak minimized in {time_:.5}s, final {contrast = :.5}")
-            mode.offsets = {"I": result[0], "Q": result[1]}  # update mode offsets
+        return result if result is not None else (None, None)
 
     def _tune_sb(self, mode: Mode, job: QmJob, center_idx: int, floor: float) -> None:
         """ """
-        logger.info(f"Minimizing {mode} sideband leakage...")
 
         def objective_fn(offsets: tuple[float]) -> float:
             correction_matrix = QMConfig.get_mixer_correction_matrix(*offsets)
             job.set_element_correction(mode.name, correction_matrix)
-            _, amps = self._sa.sweep()
-            contrast = amps[center_idx] - floor
+            contrast = self._get_contrast(center_idx, floor)
             return contrast
 
-        start_time = time.perf_counter()
         result = self._minimize(objective_fn)
-        if result is not None:
-            time_ = time.perf_counter() - start_time
-            contrast = objective_fn(result)
-            logger.success(f"SB leak minimized in {time_:.5}s, final {contrast = :.5}")
-            mode.offsets = {"G": result[0], "P": result[1]}  # update mode offsets
+        return result if result is not None else (None, None)
 
     def _check_tuning(self, center: float) -> tuple[bool, int, float]:
         """ """
@@ -120,11 +107,27 @@ class MixerTuner:
         logger.info(f"Tuning check at {real_center:E}: {contrast = :.5}dBm")
         return is_tuned, center_idx, floor
 
+    def _get_qua_program(self, mode: Mode) -> _Program:
+        """ """
+        with program() as mixer_tuning:
+            with infinite_loop_():
+                mode.play("constant_pulse")
+        return mixer_tuning
+
+    def _get_contrast(self, center_idx: int, floor: float) -> float:
+        """ """
+        _, amps = self._sa.sweep()
+        return abs(amps[center_idx] - floor)
+
     def _minimize(self, fn: Callable[[tuple[float]], float]) -> tuple[float]:
         """ """
-        opt = {"fatol": self.threshold, "initial_simplex": self.simplex}
+        start_time = time.perf_counter()
+        fatol, simplex, maxiter = self.threshold, self.simplex, self.maxiter
+        opt = {"fatol": fatol, "initial_simplex": simplex, "maxiter": maxiter}
         result = scipy.optimize.minimize(fn, [0, 0], method="Nelder-Mead", options=opt)
         if result.success:
+            time_, contrast = time.perf_counter() - start_time, fn(result.x)
+            logger.success(f"Minimized in {time_:.5}s with final {contrast = :.5}")
             return result.x
         else:
             logger.warning(f"Minimization unsuccessful, details: {result.message}")

75e32402e78cd8e438ab8124297c677ab690a1a8
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-10 13:13:48 +0800

    Write example scripts showing use of stagehand

--- qcrew/measure/freq_sweep.py
+++ qcrew/measure/freq_sweep.py
@@ -1,12 +1,13 @@
 """ """
 
-from qcrew.control.instruments.mixer_tuner import MixerTuner
 from qcrew.control.stage import Stagehand
+
+# insert helper method to plot and show sweep
 
 if __name__ == " __main__":
     stghand = Stagehand()  # init instrument client to mediate between server and user
     stg = stghand.link("QUBIT", "RR", "SA")  # stage dict contains instrument proxies
     qubit, rr, sa = stg["QUBIT"], stg["RR"], stg["SA"]  # unpack stage dict
-    qm = stghand.get_qm(qubit, rr)  # returns an already configured QM
-    mxrtnr = MixerTuner(sa, qm, qubit, rr)  # initialize the new improved MixerTuner
-    mxrtnr.tune()  # tunes both lo and sb only if out of tune in 5s per mode
+    freq1, amp1 = sa.sweep(center=qubit.lo_freq, span=250e6)
+    freq2, amp2 = sa.sweep(center=rr.lo_freq, span=250e6)
+    # call helper method to plot and show sweep

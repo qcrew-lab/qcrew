c63d16df9deb5a199b681acaac8a9905d45b3b41
Atharv Joshi <general.qcrew@gmail.com>
2021-07-12 19:40:49 +0800

    Inconsequential change

--- qcrew/measure/freq_sweep.py
+++ qcrew/measure/freq_sweep.py
@@ -8,6 +8,4 @@ if __name__ == " __main__":
     stghand = Stagehand()  # init instrument client to mediate between server and user
     stg = stghand.link("QUBIT", "RR", "SA")  # stage dict contains instrument proxies
     qubit, rr, sa = stg["QUBIT"], stg["RR"], stg["SA"]  # unpack stage dict
-    freq1, amp1 = sa.sweep(center=qubit.lo_freq, span=250e6)
-    freq2, amp2 = sa.sweep(center=rr.lo_freq, span=250e6)
     # call helper method to plot and show sweep

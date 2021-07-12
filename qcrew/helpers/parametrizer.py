19453567bdb766873b172700ec9ad4f26e855e46
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-12 21:29:03 +0800

    Get params recursively for Parametrized objects nested in sequences and mappings

--- qcrew/helpers/parametrizer.py
+++ qcrew/helpers/parametrizer.py
@@ -23,9 +23,25 @@ class Parametrized(Yamlable):
                 cls_ = type(self).__name__  # get subclass name
                 logger.exception(f"Parameter '{param}' must be an attribute of {cls_}")
                 raise SystemExit("Failed to get parameters, exiting...") from e
-            else:
+            else:  # get params recursively if Parametrized objects found in param_value
                 if isinstance(param_value, Parametrized):
-                    param_dict[param] = param_value.parameters  # get params recursively
+                    param_dict[param] = param_value.parameters
+                elif isinstance(param_value, (list, tuple, set, frozenset)):
+                    param_list = list()
+                    for value in param_value:
+                        if isinstance(value, Parametrized):
+                            param_list.append(value.parameters)
+                        else:
+                            param_list.append(value)
+                    param_dict[param] = param_list
+                elif isinstance(param_value, dict):
+                    nested_param_dict = dict()
+                    for key, value in param_value.items():
+                        if isinstance(value, Parametrized):
+                            nested_param_dict[key] = value.parameters
+                        else:
+                            nested_param_dict[key] = value
+                        param_dict[param] = nested_param_dict
                 else:
                     param_dict[param] = param_value
         return param_dict

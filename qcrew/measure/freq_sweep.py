b9d22d8eca2d3c10de722adec0ab1b00b0653abd
Atharv Joshi <general.qcrew@gmail.com>
2021-07-16 10:37:24 +0800

    Write basic meas script for freq sweep

--- qcrew/measure/freq_sweep.py
+++ qcrew/measure/freq_sweep.py
@@ -1,11 +1,32 @@
 """ """
 
-from qcrew.control.stage import Stagehand
+import matplotlib.pyplot as plt
+from qm import qua
+from qcrew.control.modes.mode import Mode
+from qcrew.control.stage.stagehand import Stagehand
 
-# insert helper method to plot and show sweep
+
+def get_qua_program(mode: Mode):
+    with qua.program() as play_constant_pulse:
+        with qua.infinite_loop_():
+            mode.play("constant_pulse")
+    return play_constant_pulse
+
 
 if __name__ == " __main__":
-    stghand = Stagehand()  # init instrument client to mediate between server and user
-    stg = stghand.link("QUBIT", "RR", "SA")  # stage dict contains instrument proxies
-    qubit, rr, sa = stg["QUBIT"], stg["RR"], stg["SA"]  # unpack stage dict
-    # call helper method to plot and show sweep
+
+    with Stagehand() as stage:
+        qubit, rr, sa = stage.QUBIT, stage.RR, stage.SA
+
+        qubit.lo_freq = 5e9
+        qubit.int_freq = -50e6
+
+        rr.lo_freq = 8e9
+        rr.int_freq = -100e6
+
+        qm = stage.QM
+        job = qm.execute(get_qua_program(qubit))
+
+        freqs, amps = sa.sweep(center=qubit.lo_freq, span=250e6)
+        plt.plot(freqs, amps)
+        plt.show()

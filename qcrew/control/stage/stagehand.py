8088bf61382f450a282ce5a9a8b8ef7da936efc8
Atharv Joshi <general.qcrew@gmail.com>
2021-07-16 10:36:50 +0800

    Update Stagehand design

--- qcrew/control/stage/stagehand.py
+++ qcrew/control/stage/stagehand.py
@@ -3,9 +3,16 @@
 from pathlib import Path
 
 import Pyro5.api as pyro
-from qcrew.control.stage.stage import RemoteStage, Stage
+from qcrew.control.stage.stage import LocalStage, RemoteStage
 from qcrew.control.stage.stage_setup import LOCAL_CONFIGPATH
 from qcrew.helpers import logger
+
+# pylint: disable=unused-import, these classes need to be imported for yamlizing to work
+
+from qcrew.control.modes.qubit import Qubit
+from qcrew.control.modes.readout import Readout
+
+# pylint: enable=unused-import
 
 
 class Stagehand:
@@ -14,43 +21,40 @@ class Stagehand:
     def __init__(self, configpath: Path = LOCAL_CONFIGPATH) -> None:
         """ """
         self._configpath = configpath
-        self.stage: Stage = Stage(configpath=self._configpath, has_qm=True)
+        self.stage: LocalStage = LocalStage(configpath=self._configpath)
+        self.proxies = dict()  # update by _stage_remote_objects()
 
-    def __enter__(self) -> Stage:
+    def __enter__(self) -> LocalStage:
         """ """
         self._stage_local_objects()
-        self._stage.remote_objects()
+        self._stage_remote_objects()
         return self.stage
 
     def _stage_local_objects(self) -> None:
         """ """
-        local_config = self.stage.config
-        for local_object_name, local_object in local_config.items():
-            setattr(self.stage, local_object_name, local_object)
-            logger.success(f"Set stage attribute '{local_object_name}'")
+        for mode in self.stage.modes:
+            setattr(self.stage, mode.name, mode)
+            logger.success(f"Set stage attribute '{mode.name}'")
 
     def _stage_remote_objects(self) -> None:
         """ """
         with pyro.Proxy(RemoteStage.get_uri()) as remote_stage:
-            instruments = remote_stage.instruments
-            logger.success(f"Located {len(instruments)} remote instruments")
-            instrument_ids = tuple(instrument.id for instrument in instruments)
+
+            for name, uri in remote_stage.services.items():
+                instrument_proxy = pyro.Proxy(uri)
+                logger.success(f"Connected to remote instrument with {name = }")
+                self.proxies[name] = instrument_proxy
+                setattr(self.stage, name, instrument_proxy)
+                logger.success(f"Set stage attribute '{name}'")
 
             for mode in self.stage.modes:
-                id_ = mode.lo
-                if id_ in instrument_ids:
-                    proxy = instruments[instrument_ids.index(id_)]
-                    mode.lo = proxy
-                    logger.success(f"Linked {mode.name} & {proxy.name} with id = {id_}")
-
-            for name, proxy in remote_stage.config.items():
-                setattr(self.stage, name, proxy)
-                logger.success(f"Set stage attribute '{name}'")
+                lo_name = mode.lo
+                if lo_name in self.proxies:
+                    mode.lo = self.proxies[lo_name]
+                    logger.success(f"Connected {lo_name} as {mode.name} lo")
 
     def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
         """ """
-        self.stage.save()
-        logger.info("Releasing proxies...")
-        with pyro.Proxy(RemoteStage.get_uri()) as remote_stage:
-            for proxy in remote_stage.config.values():
-                proxy._pyroRelease()
+        for mode in self.stage.modes:
+            mode.lo = mode.lo.name
+        self.stage.teardown()

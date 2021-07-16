9e8c2282d748be4fc04722b9dba284f6c3b8e11e
Atharv Joshi <general.qcrew@gmail.com>
2021-07-16 14:17:29 +0800

    Add a 5s delay to inform user

--- qcrew/control/stage/stage_teardown.py
+++ qcrew/control/stage/stage_teardown.py
@@ -1,8 +1,13 @@
 """ """
 
+import time
+
 import Pyro5.api as pyro
 from qcrew.control.stage.stage import RemoteStage
+from qcrew.helpers import logger
 
 if __name__ == "__main__":
+    logger.info("Tearing down remote stage, please wait ~5s...")
     with pyro.Proxy(RemoteStage.get_uri()) as remote_stage:
         remote_stage.teardown()
+    time.sleep(5)

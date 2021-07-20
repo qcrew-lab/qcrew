8d7d76440d6e394801e2999d75e33538be5291c0
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-20 13:13:31 +0800

    Create separate module and classes for supporting readout pulse integration weights

--- qcrew/control/pulses/__init__.py
+++ qcrew/control/pulses/__init__.py
@@ -1,5 +1,6 @@
 """ """
 
+from qcrew.control.pulses.integration_weights import ConstantIntegrationWeights
 from qcrew.control.pulses.pulse import Pulse
 from qcrew.control.pulses.constant_pulse import ConstantPulse
 from qcrew.control.pulses.gaussian_pulse import GaussianPulse

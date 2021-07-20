b10d532fcbc57903c93ae92eeb5afc321b0b60b2
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-20 20:06:49 +0800

    Slightly alter logging message

--- qcrew/control/stage/stage_teardown.py
+++ qcrew/control/stage/stage_teardown.py
@@ -7,7 +7,7 @@ from qcrew.control.stage.stage import RemoteStage
 from qcrew.helpers import logger
 
 if __name__ == "__main__":
-    logger.info("Tearing down remote stage, please wait ~3s...")
+    logger.info("Tearing down remote stage, no action needed, just wait ~3s...")
     with pyro.Proxy(RemoteStage.get_uri()) as remote_stage:
         remote_stage.teardown()
     time.sleep(3)

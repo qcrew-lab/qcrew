7943b6cfd210a9649691eeee435c9a18e8a2ce9a
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-20 13:14:02 +0800

    Update in line with changes to integ weights

--- qcrew/control/pulses/pulse.py
+++ qcrew/control/pulses/pulse.py
@@ -4,14 +4,16 @@ from numbers import Real
 from typing import ClassVar
 
 import numpy as np
+
 from qcrew.helpers import logger
 from qcrew.helpers.parametrizer import Parametrized
-from qcrew.helpers.yamlizer import Yamlable
+from qcrew.helpers.yamlizer import Yamlizable
 
 BASE_PULSE_AMP = 0.2  # in V
 CLOCK_CYCLE = 4  # in ns
 
-class Pulse(Parametrized, Yamlable):
+
+class Pulse(Parametrized, Yamlizable):
     """ """
 
     _parameters: ClassVar[set[str]] = {"length", "ampx"}
@@ -24,14 +26,16 @@ class Pulse(Parametrized, Yamlable):
         integration_weights=None,
     ) -> None:
         """ """
-        self._length: int = length
-        self.ampx: float = ampx
         self.is_readout_pulse: bool = False
         self.integration_weights = None
 
         if integration_weights is not None:
             self.integration_weights = integration_weights
             self.is_readout_pulse = True
+
+        self.length: int = None  # length and ampx will be set by __call__()
+        self.ampx: float = None
+        self(length=length, ampx=ampx)
 
         self.has_mix_waveforms: bool = True  # qcrew's default use case
 
@@ -45,44 +49,26 @@ class Pulse(Parametrized, Yamlable):
             is_attribute = hasattr(self, name)
             if is_attribute and value is not None:
                 setattr(self, name, value)
-                if name == "_length" and self.is_readout_pulse:  # set constant iw len
-                    new_iw_len = int(self._length / CLOCK_CYCLE)
-                    logger.debug(f"Setting integration weights len = {new_iw_len}...")
-                    self.integration_weights(ampx=1 / BASE_PULSE_AMP, length=new_iw_len)
+                if self.is_readout_pulse:
+                    self._update_integration_weights(name)
             elif not is_attribute:
                 cls_name = type(self).__name__
                 logger.warning(f"Parameter '{name}' must be an attribute of {cls_name}")
         logger.success(f"Set {self}")
 
-    @property  # pulse length getter
-    def length(self) -> int:
+    def _update_integration_weights(self, key: str) -> None:
         """ """
-        return self._length
+        do_update = key == "length" and self.integration_weights.is_pinned
+        if do_update:
+            new_iw_len = int(self.length / CLOCK_CYCLE)
+            self.integration_weights.length = new_iw_len
+            logger.debug(f"Set integration weights len = {new_iw_len}")
 
     @property  # waveform amplitude samples getter
     def samples(self) -> tuple[np.ndarray]:
         """ """
         logger.error("Abstract method must be implemented by subclass(es)")
         raise NotImplementedError("Can't call `samples` on Pulse instance")
-
-    # NOTE TODO this is hard coded to return constant integration weights !!!
-    @property  # integration weights samples getter
-    def integration_weights_samples(self) -> dict[str, dict[str, np.ndarray]]:
-        """ """
-        if self.integration_weights is not None:
-            samples = self.integration_weights.samples
-            return {
-                "iwI": {
-                    "cosine": samples[0],
-                    "sine": samples[1],
-                },
-                "iwQ": {
-                    "cosine": samples[1],
-                    "sine": samples[0],
-                },
-            }
-        else:
-            logger.warning(f"No integration weights defined for {self}")
 
     @property  # pulse type_ getter for building QM config
     def type_(self) -> str:

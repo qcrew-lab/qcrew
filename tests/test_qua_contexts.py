3457be130eaa1ef2686db1b4dac6909310bb01b0
Atharv Joshi <atharvjoshi@ymail.com>
2021-07-20 13:13:08 +0800

    Add qua context test (WIP)

---
+++ tests/test_qua_contexts.py
@@ -0,0 +1,6 @@
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+from qm import qua
+
+qmm = QuantumMachinesManager()
+sim_config = SimulationConfig(duration=1000)

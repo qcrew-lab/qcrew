d8d45ee07ee1771bfcfddd32782aba69759d921d
Atharv Joshi <general.qcrew@gmail.com>
2021-07-29 16:04:11 +0800

    Set stage paths in a separate local config

--- qcrew/control/stage/stage_setup.py
+++ qcrew/control/stage/stage_setup.py
@@ -7,15 +7,10 @@ import qcrew.control.instruments as qci
 from qcrew.control.stage.stage import RemoteStage
 from qcrew.helpers import logger
 from qcrew.helpers.parametrizer import Parametrized
+import qcrew.helpers.yamlizer as yml
 
-
-LOCAL_CONFIGPATH = Path("C:/Users/qcrew/qcrew/qcrew/configs/coax_a/modes.yml")
-""" Path to the yml config containing modes to be instantiated locally """
-
-
-REMOTE_CONFIGPATH = Path("C:/Users/qcrew/qcrew/qcrew/configs/coax_a/instruments.yml")
-""" Path to the yml config containing instruments to be served remotely """
-
+_CONFIGPATH = Path("C:/Users/qcrew/qcrew/qcrew/config/stagepaths.yml")
+_REMOTE_CONFIGPATH = Path(yml.load(_CONFIGPATH)["remote"])
 
 if __name__ == "__main__":
     remote_classes = {Parametrized, qci.Instrument, qci.LabBrick, qci.Sa124}
@@ -24,7 +19,7 @@ if __name__ == "__main__":
     logger.debug(f"Exposed qcrew classes: {[cls_.__name__ for cls_ in remote_classes]}")
 
     daemon_ = pyro.Daemon(port=RemoteStage.port_num)
-    stage = RemoteStage(daemon=daemon_, configpath=REMOTE_CONFIGPATH)
+    stage = RemoteStage(daemon=daemon_, configpath=_REMOTE_CONFIGPATH)
     self_uri = daemon_.register(stage, objectId=stage.servername)
     logger.success(f"Served remote stage at {self_uri}")
 

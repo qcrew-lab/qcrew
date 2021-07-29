cbe9c39bc26a653fe6b3e2c5af3e9021b6f0834b
Atharv Joshi <general.qcrew@gmail.com>
2021-07-29 15:51:17 +0800

    Ignore local config folder which will have .yml and .bat files

--- scripts/setup_stage.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-TITLE "QCREW'S REMOTE STAGE"
-@echo on
-call C:\Users\qcrew\Anaconda3\Scripts\activate.bat
-call conda activate qcrew-run
-cd ../qcrew/control/stage
-python -m stage_setup

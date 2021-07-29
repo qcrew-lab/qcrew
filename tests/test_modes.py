0c88a7cd5784a8bb5f9483b03650611cbf801223
Atharv Joshi <general.qcrew@gmail.com>
2021-07-29 17:19:51 +0800

    Fix configpath bug

--- tests/test_modes.py
+++ tests/test_modes.py
@@ -4,11 +4,11 @@ import pathlib
 import pprint
 
 import qcrew.helpers.yamlizer as yml
-from tests.test_labbrick import TestLabBrick
+from test_labbrick import TestLabBrick
 from qcrew.control.modes import Qubit, Readout
 from qcrew.helpers import logger
 
-CONFIGPATH = pathlib.Path.cwd() / "tests/test_modes.yml"
+CONFIGPATH = pathlib.Path.cwd() / "test_modes.yml"
 
 modes = yml.load(CONFIGPATH)
 logger.success(f"Loaded {len(modes)} test modes from {CONFIGPATH}")

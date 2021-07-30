81d564ccce110aa27eae0701929badcc28217e32
Atharv Joshi <general.qcrew@gmail.com>
2021-07-30 14:42:34 +0800

    changed some input parameters

--- qcrew/measure/power_rabi.py
+++ qcrew/measure/power_rabi.py
@@ -15,15 +15,11 @@ import qua_macros as macros
 
 
 class PowerRabi(Experiment):
-    def __init__(
-        self, qubit_mode, rr_mode, qubit_op, readout_op, fit_fn=None, **other_params
-    ):
+    def __init__(self, modes, qubit_op, fit_fn="sine", **other_params):
 
         # Get attributes
-        self.qubit_mode = qubit_mode
-        self.rr_mode = rr_mode
+        self.modes = modes
         self.qubit_op = qubit_op
-        self.readout_op = readout_op
         self.fit_fn = fit_fn
 
         # Passes remaining parameters to parent
@@ -33,11 +29,12 @@ class PowerRabi(Experiment):
         """
         Defines pulse sequence to be played inside the experiment loop
         """
+        qubit, rr = self.modes
 
-        self.qubit_mode.play(self.qubit_op, ampx=self.x)
-        qua.align(self.qubit_mode.name, self.rr_mode.name)
-        self.rr_mode.measure((self.I, self.Q))
-        qua.wait(int(self.wait_time // 4), self.qubit_mode.name)
+        qubit.play(self.qubit_op, ampx=self.x)
+        qua.align(qubit.name, rr.name)
+        rr.measure((self.I, self.Q))
+        qua.wait(int(self.wait_time // 4), qubit.name)
         macros.stream_results(self.var_list)
 
 
@@ -48,22 +45,17 @@ if __name__ == "__main__":
     #############        SETTING EXPERIMENT      ################
     with Stagehand() as stage:
 
-        qubit, rr = stage.QUBIT, stage.RR
-
         exp_params = {
-            "qubit_mode": qubit,
-            "rr_mode": rr,
-            "reps": 200000,  # number of sweep repetitions
-            "wait_time": 32000,  # delay between reps in ns, an integer multiple of 4 >= 16
+            "modes": [stage.QUBIT, stage.RR],
+            "reps": 200000,
+            "wait_time": 32000,
             "x_sweep": (
                 -1.9,
                 1.9 + 0.2 / 2,
                 0.2,
-            ),  # x sweep is set by start, stop, and step
-            # "y_sweep": [True, False],  # x sweep is set by start, stop, and step
-            "qubit_op": "gaussian_pulse",  # Operations to be used in the exp.
-            "readout_op": "readout",
-            "fit_fn": "sine",  # name eof the fit function
+            ),
+            # "y_sweep": [True, False],
+            "qubit_op": "gaussian_pulse",
         }
 
         experiment = PowerRabi(**exp_params)

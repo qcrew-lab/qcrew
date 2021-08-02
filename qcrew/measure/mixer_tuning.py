8f9f8050b943f464864558c7d10466155a13298e
Atharv Joshi <general.qcrew@gmail.com>
2021-08-02 15:37:42 +0800

    Tune for new lo and int freq

--- qcrew/measure/mixer_tuning.py
+++ qcrew/measure/mixer_tuning.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
         qubit, rr, sa = stage.QUBIT, stage.RR, stage.SA
 
         # set new carrier and intermediate frequencies to the modes
-        qubit.lo_freq = 6e9
-        qubit.int_freq = -100e6
-        rr.lo_freq = 7e9
-        rr.int_freq = -10e6
+        qubit.lo_freq = 5.0175e9
+        qubit.int_freq = -50e6
+        rr.lo_freq = 8.60385e9
+        rr.int_freq = -50e6
 
         # get an already configured qm after making changes to modes
         qm = stage.QM

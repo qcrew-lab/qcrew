a0b29bfd9e06011ccbae4b15d70c6071d5bcc662
Atharv Joshi <general.qcrew@gmail.com>
2021-08-02 10:27:43 +0800

    power_rabi minor rewriting

--- qcrew/measure/power_rabi.py
+++ qcrew/measure/power_rabi.py
@@ -1,8 +1,6 @@
 """
-A python class describing a power rabi measurement using the QM 
-system. 
-This class serves as a QUA script generator with user-defined parameters. It 
-also defines how the information is retrieved from result handles.
+A python class describing a power rabi measurement using QM.
+This class serves as a QUA script generator with user-defined parameters.
 """
 # --------------------------------- Imports ------------------------------------
 from qm import qua
@@ -73,73 +71,6 @@ if __name__ == "__main__":
 
         power_rabi = experiment.QUA_sequence()
 
-        ###################        RUN MEASUREMENT        ############################
+        #################   RUN MEASUREMENT   ##################
 
         job = stage.QM.execute(power_rabi)
-
-    """
-    ####################        INVOKE HELPERS        ###########################
-    # fetch helper and plot hepler
-
-    fetcher = Fetcher(handle=job.result_handles, num_results=experiment.reps)
-    plotter = Plotter(title=EXP_NAME, xlabel="Amplitude scale factor")
-    stats = (None, None, None)  # to hold running stats (stderr, mean, variance * (n-1))
-
-    # initialise database under dedicated folder
-    db = initialise_database(
-        exp_name=EXP_NAME,
-        sample_name=SAMPLE_NAME,
-        project_name=PROJECT_FOLDER_NAME,
-        path=DATAPATH,
-        timesubdir=False,
-        timefilename=True,
-    )
-
-    ##################        LIVE POST-PROCESSING LOOP        ####################
-
-    with DataSaver(db) as datasaver:
-
-        ###############        SAVE MEASUREMENT RUN METADATA       ###################
-
-        datasaver.add_metadata(exp_params)
-
-        while fetcher.is_fetching:
-            ###############            FETCH PARTIAL RESULTS         ###############
-
-            (num_so_far, update_results) = fetcher.fetch()
-            if (
-                not update_results
-            ):  # empty dict return means no new results are available
-                continue
-            ################            LIVE SAVE RESULTS         ##################
-            datasaver.update_multiple_results(
-                update_results, save=["I", "Q"], group="data"
-            )
-
-            ########            CALCULATE RUNNING MEAN STANDARD ERROR         #########
-
-            zs_raw = np.sqrt(update_results["Z_SQ_RAW"])
-            zs_raw_avg = np.sqrt(update_results["Z_SQ_RAW_AVG"])
-            stats = get_std_err(zs_raw, zs_raw_avg, num_so_far, *stats)
-
-            #############            LIVE PLOT AVAILABLE RESULTS         ###########
-
-            zs = np.sqrt(update_results["Z_AVG"])  # latest batch of average signal
-            xs = update_results["x"]
-            plotter.live_plot(
-                xs, zs, num_so_far, fit_fn=experiment.fit_fn, err=stats[0]
-            )
-            time.sleep(1)  # prevent over-fetching, over-saving, ulta-fast live plotting
-
-        ##################         SAVE REMAINING DATA         #####################
-
-        # to save final average and sweep variables, we extract them from "update_results"
-        final_save_dict = {"Z_AVG": zs, "x": xs}
-        datasaver.add_multiple_results(
-            final_save_dict, save=["Z_AVG", "x"], group="data"
-        )
-
-    ##########################          fin           #############################
-
-    print(job.execution_report())
-    """

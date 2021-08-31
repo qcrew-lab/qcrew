22d383cffa3073082850c1a6aadeaedcafd01766
qcrew-lab <general.qcrew@gmail.com>
2021-08-31 14:16:15 +0800

    rr spec

--- qcrew/measure/rr_spec.py
+++ qcrew/measure/rr_spec.py
@@ -33,7 +33,7 @@ class RRSpectroscopy(Experiment):
         (rr,) = self.modes  # get the modes
 
         qua.update_frequency(rr.name, self.x)  # update resonator pulse frequency
-        rr.measure((self.I, self.Q))  # measure transmitted signal
+        rr.measure((self.I, self.Q))  # , ampx=self.y)  # measure transmitted signal
         qua.wait(int(self.wait_time // 4), rr.name)  # wait system reset
 
         self.QUA_stream_results()  # stream variables (I, Q, x, etc)
@@ -43,20 +43,23 @@ class RRSpectroscopy(Experiment):
 
 if __name__ == "__main__":
 
-    x_start = -200e6
-    x_stop = 200e6
-    x_step = 5e6
+    x_start = -51.5e6
+    x_stop = -48.5e6
+    x_step = 0.02e6
 
     parameters = {
         "modes": ["RR"],
-        "reps": 2000,
+        "reps": 3000,
         "wait_time": 10000,
         "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
+        # "y_sweep": (0.1, 0.08, 0.06, 0.04,0.02)
     }
 
     plot_parameters = {
         "xlabel": "Resonator pulse frequency (Hz)",
-        # "suptitle": "rr spectroscopy",
+        "ylabel": "Resonator pulse amplitude scaling",
+        "plot_type": "1D",
+        # "trace_labels": [0.1, 0.08, 0.06, 0.04,0.02]
     }
 
     experiment = RRSpectroscopy(**parameters)

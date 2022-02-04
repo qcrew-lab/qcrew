f66dda32b47ab93b060d719f70123f36dfb1b55a
Atharv Joshi <general.qcrew@gmail.com>
2022-02-04 15:18:16 +0800

    Write ef scripts

---
+++ qcrew/measure/ef_power_rabi.py
@@ -0,0 +1,75 @@
+"""
+A python class describing a ef power rabi measurement using QM.
+This class serves as a QUA script generator with user-defined parameters.
+"""
+
+from typing import ClassVar
+
+from qcrew.control import professor as prof
+from qcrew.measure.experiment import Experiment
+from qm import qua
+
+
+# ---------------------------------- Class -------------------------------------
+
+
+class PowerRabi(Experiment):
+
+    name = "ef_power_rabi"
+
+    _parameters: ClassVar[set[str]] = Experiment._parameters | {
+        "qubit_op",  # operation used for exciting the qubit
+        "fit_fn",  # fit function
+    }
+
+    def __init__(
+        self, qubit_op, qubit_pi_pulse_name, ef_int_freq, fit_fn="sine", **other_params
+    ):
+
+        self.qubit_op = qubit_op
+        self.fit_fn = fit_fn
+        self.qubit_pi_pulse_name = qubit_pi_pulse_name
+        self.ef_int_freq = ef_int_freq
+
+        super().__init__(**other_params)  # Passes other parameters to parent
+
+    def QUA_play_pulse_sequence(self):
+        """
+        Defines pulse sequence to be played inside the experiment loop
+        """
+        qubit, rr = self.modes  # get the modes
+
+        qubit.play(self.qubit_pi_pulse_name)
+        qua.update_frequency(qubit.name, self.ef_int_freq)
+        qubit.play(self.qubit_op, ampx=self.x)  # play qubit pulse
+        qua.update_frequency(qubit.name, qubit.int_freq)
+        qubit.play(self.qubit_pi_pulse_name)
+        qua.align(qubit.name, rr.name)  # wait qubit pulse to end
+        rr.measure((self.I, self.Q))  # measure qubit state
+        qua.wait(int(self.wait_time // 4), rr.name)  # wait system reset
+
+        self.QUA_stream_results()  # stream variables (I, Q, x, etc)
+
+
+# -------------------------------- Execution -----------------------------------
+
+if __name__ == "__main__":
+
+    parameters = {
+        "modes": ["QUBIT", "RR"],
+        "reps": 10000,
+        "wait_time": 40000,
+        "ef_int_freq": -90e6,
+        "qubit_pi_pulse_name": "pi",
+        "x_sweep": (-1.5, 1.5 + 0.02 / 2, 0.02),
+        "qubit_op": "gaussian_pulse",
+    }
+
+    plot_parameters = {
+        "xlabel": "Qubit pulse amplitude scaling",
+    }
+
+    experiment = PowerRabi(**parameters)
+    experiment.setup_plot(**plot_parameters)
+
+    prof.run(experiment)

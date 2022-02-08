365d514ce17c239ec39f4a1578fe400cd52301cf
Atharv Joshi <general.qcrew@gmail.com>
2022-02-08 16:34:52 +0800

    write new scripts - qubit population, repeat power rabi, t2 echo

---
+++ qcrew/measure/T2_echo.py
@@ -0,0 +1,76 @@
+"""
+A python class describing a T2 measurement using QM.
+This class serves as a QUA script generator with user-defined parameters.
+"""
+
+from typing import ClassVar
+
+from qcrew.control import professor as prof
+from qcrew.measure.experiment import Experiment
+from qm import qua
+
+# ---------------------------------- Class -------------------------------------
+
+
+class T2_Echo(Experiment):
+
+    name = "T2_echo"
+
+    _parameters: ClassVar[set[str]] = Experiment._parameters | {
+        "qubit_pi2",  # operation used for exciting the qubit
+        "qubit_pi",
+        "fit_fn",  # fit function
+        "detuning",  # qubit pulse detuning
+    }
+
+    def __init__(
+        self, qubit_pi2, qubit_pi, detuning=0, fit_fn="exp_decay_sine", **other_params
+    ):
+
+        self.qubit_pi2 = qubit_pi2
+        self.qubit_pi = qubit_pi  # half pi pulse
+        self.fit_fn = fit_fn
+        self.detuning = detuning  # frequency detuning of qubit operation
+
+        super().__init__(**other_params)  # Passes other parameters to parent
+
+    def QUA_play_pulse_sequence(self):
+        """
+        Defines pulse sequence to be played inside the experiment loop
+        """
+        qubit, rr = self.modes  # get the modes
+        qua.update_frequency(qubit.name, qubit.int_freq + self.detuning)  # detune
+        qubit.play(self.qubit_pi2)  # play half pi qubit pulse
+        qua.wait(self.x / 2, qubit.name)  # wait for partial qubit decay
+        qubit.play(self.qubit_pi)  # play pi qubit pulse
+        qua.wait(self.x / 2, qubit.name)  # wait for partial qubit decay
+        qubit.play(self.qubit_pi2)  # play half pi qubit pulse
+        qua.align(qubit.name, rr.name)  # wait last qubit pulse to end
+        rr.measure((self.I, self.Q))  # measure qubit state
+        qua.wait(int(self.wait_time // 4), rr.name)  # wait system reset
+
+        self.QUA_stream_results()  # stream variables (I, Q, x, etc)
+
+
+# -------------------------------- Execution -----------------------------------
+
+if __name__ == "__main__":
+
+    parameters = {
+        "modes": ["QUBIT", "RR"],
+        "reps": 20000,
+        "wait_time": 100000,
+        "x_sweep": (int(16), int(10000 + 200 / 2), int(200)),
+        "qubit_pi2": "pi2",
+        "qubit_pi": "pi",
+        "detuning": int(0),
+    }
+
+    plot_parameters = {
+        "xlabel": "Relaxation time (clock cycles)",
+    }
+
+    experiment = T2_Echo(**parameters)
+    experiment.setup_plot(**plot_parameters)
+
+    prof.run(experiment)

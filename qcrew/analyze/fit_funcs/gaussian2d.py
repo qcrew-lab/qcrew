2638315bc9a425f3efaf042a1cd9d3bfeb163add
Atharv Joshi <general.qcrew@gmail.com>
2022-02-21 10:40:04 +0800

    changed xspan and yspan guesses to make more sense with our data shape

--- qcrew/analyze/fit_funcs/gaussian2d.py
+++ qcrew/analyze/fit_funcs/gaussian2d.py
@@ -2,29 +2,32 @@ import numpy as np
 
 
 def func(xs, ys, x0=0, y0=0, sigmax=1, sigmay=1, area=1, ofs=0):
-    r2 = (xs - x0)**2 / (2 * sigmax**2) + (ys - y0)**2 / (2 * sigmay**2)
+    r2 = (xs - x0) ** 2 / (2 * sigmax ** 2) + (ys - y0) ** 2 / (2 * sigmay ** 2)
     return ofs + area / (2 * sigmax * sigmay) / np.sqrt(np.pi / 2) * np.exp(-r2)
 
 
 def guess(xs, ys, zs):
-    zofs = np.mean([zs[0,:], zs[-1,:], zs[:,0], zs[:,-1]])
+    zofs = np.mean([zs[0, :], zs[-1, :], zs[:, 0], zs[:, -1]])
     zs = zs - zofs
     maxidxy = np.argmax(np.abs(zs).sum(axis=1))
     maxidxx = np.argmax(np.abs(zs).sum(axis=0))
 
-    xspan = xs[-1,0] - xs[0,0]
-    yspan = ys[0,-1] - ys[0,0]
+    xspan = xs[0, -1] - xs[0, 0]
+    yspan = ys[-1, 0] - ys[0, 0]
+    print("xspan, ", xspan)
     sigmax = xspan / 5
     sigmay = yspan / 5
 
     maxidx0 = np.argmax(np.abs(zs))
     dmin = (np.max(xs) - np.min(xs)) / 8
-    mask = ((xs - xs.flatten()[maxidxx])**2 + (ys - ys.flatten()[maxidx0])**2) > dmin**2
+    mask = (
+        (xs - xs.flatten()[maxidxx]) ** 2 + (ys - ys.flatten()[maxidx0]) ** 2
+    ) > dmin ** 2
     area = np.sum(zs[mask])
 
     return dict(
-        x0=xs[maxidxy,maxidxx],
-        y0=ys[maxidxy,maxidxx],
+        x0=xs[maxidxy, maxidxx],
+        y0=ys[maxidxy, maxidxx],
         ofs=zofs,
         area=area,
         sigmax=sigmax,

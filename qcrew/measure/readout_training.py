8cdcbd98b3bee0f4ca23e91feb1b6c98970c439b
Atharv Joshi <general.qcrew@gmail.com>
2022-02-21 09:21:49 +0800

    working readout training script

--- qcrew/measure/readout_training.py
+++ qcrew/measure/readout_training.py
@@ -12,18 +12,28 @@ if __name__ == "__main__":
         rr, qubit = stage.RR, stage.QUBIT
 
         params = {
-            "reps": 1000,
-            "wait_time": 100000,  # ns
+            "reps": 10000,
+            "wait_time": 500000,  # ns
             "readout_pulse": "readout_pulse",  # pulse to be trained
             "qubit_pi_pulse": "pi",  # pulse to excite qubit
             "weights_file_path": Path("C:/Users/qcrew/Desktop/qcrew/qcrew/config")
-            / "opt_readout_weights_F.npz",
+            / "opt_weights_.npz",
         }
 
+        # import time
+        # print(time.localtime().tm_hour)
+        # import datetime
+        # datetime.datetime.now()
         # get an already configured qm after making changes to modes
         qm = stage.QM
         ro_trainer = ReadoutTrainer(rr, qubit, qm, params)
-        env_g, env_e, weights, squeezed_weights = ro_trainer.train()
+        env_g, env_e = ro_trainer.train()
+
+        plt.plot(np.abs(env_g - env_e))
+        plt.show()
+        plt.plot(np.angle(env_g - env_e))
+        plt.show()
+
         plt.plot(np.real(env_g))
         plt.plot(np.imag(env_g))
         plt.show()
@@ -32,5 +42,10 @@ if __name__ == "__main__":
         plt.plot(np.imag(env_e))
         plt.show()
 
-        print(weights.shape)
-        print(squeezed_weights.shape)
+        plt.scatter(np.real(env_g), np.imag(env_g))
+        plt.scatter(np.real(env_e), np.imag(env_e))
+        for i in range(len(env_e[400:500])):
+            plt.plot(
+                [np.real(env_e)[400 + i], np.real(env_g)[400 + i]],
+                [np.imag(env_e)[400 + i], np.imag(env_g)[400 + i]],
+            )

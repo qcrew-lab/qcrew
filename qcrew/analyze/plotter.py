3e93a989026c3e25d6fa59542f89fdd849ec5f97
Atharv Joshi <general.qcrew@gmail.com>
2022-02-22 16:25:25 +0800

    added colorbars to 2d plots, zlog and zlimits

--- qcrew/analyze/plotter.py
+++ qcrew/analyze/plotter.py
@@ -1,6 +1,7 @@
 """ Qcrew plotter v1.0 """
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from qcrew.helpers import logger
 from IPython import display
 from qcrew.analyze import fit
@@ -45,7 +46,6 @@ class Plotter:
         plt.rcParams["legend.fontsize"] = 12
 
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1, 1, 1)
         self.hdisplay = display.display(self.fig, display_id=True)
 
     def fit(self, xs, ys, fit_fn) -> tuple:
@@ -76,7 +76,8 @@ class Plotter:
         This version of the plotter assumes 1 dependent variable and up to 2 independent variables.
         """
 
-        self.ax.clear()
+        self.fig.clear()
+        ax = self.fig.gca()
 
         # Determine how to plot the data
         if self.plot_setup["plot_type"] == "1D":
@@ -121,8 +122,8 @@ class Plotter:
                     )
 
             # Set plot parameters
-            self.ax.set_ylabel(self.plot_setup["zlabel"])
-            self.ax.legend()
+            ax.set_ylabel(self.plot_setup["zlabel"])
+            ax.legend()
 
         if self.plot_setup["plot_type"] == "2D":
             if len(independent_data) != 2:
@@ -136,46 +137,70 @@ class Plotter:
             self.plot_2D(x_data, y_data, z_data)
 
         # Set plot parameters
-        self.ax.set_title(
+        ax.set_title(
             self.plot_setup["title"] + f": {n} repetition{'s' if n > 1 else ''}"
         )
-        self.ax.set_xlabel(self.plot_setup["xlabel"])
+        ax.set_xlabel(self.plot_setup["xlabel"])
         self.hdisplay.update(self.fig)
 
     def plot_2D(self, x, y, z):
 
-        im = self.ax.pcolormesh(x, y, z, shading="auto", cmap=self.plot_setup["cmap"])
-        # cbar = self.fig.colorbar(im)
+        if self.plot_setup["zlog"]:
+            if self.plot_setup["zlimits"]:
+                vmin = self.plot_setup["zlimits"][0]
+                vmax = self.plot_setup["zlimits"][1]
+                norm = colors.LogNorm(vmin=vmin, vmax=vmax)
+            else:
+                norm = colors.LogNorm(vmin=z.min(), vmax=z.max())
+        else:
+            if self.plot_setup["zlimits"]:
+                vmin = self.plot_setup["zlimits"][0]
+                vmax = self.plot_setup["zlimits"][1]
+                norm = colors.Normalize(vmin=vmin, vmax=vmax)
+            else:
+                norm = None
+
+        ax = self.fig.gca()
+        im = ax.pcolormesh(
+            x, y, z, norm=norm, shading="auto", cmap=self.plot_setup["cmap"]
+        )
+        cbar = self.fig.colorbar(im)
         # Set plot parameters
-        self.ax.set_ylabel(self.plot_setup["ylabel"])
-        # cbar.set_label(self.plot_setup["zlabel"])
+        ax.set_ylabel(self.plot_setup["ylabel"])
+        cbar.set_label(self.plot_setup["zlabel"])
 
     def plot_1D(self, x, z, fit_fn, label=None, err=None, color="b"):
+
+        ax = self.fig.gca()
 
         # plot the data
         label = label or "data"
         if err is not None and self.plot_setup["plot_err"]:
-            self.plot_errorbar(x, z, self.ax, err, label, color=color)
+            self.plot_errorbar(x, z, ax, err, label, color=color)
         else:
-            self.plot_scatter(x, z, self.ax, label, color=color)
+            self.plot_scatter(x, z, ax, label, color=color)
 
         if fit_fn:
             # plot the fit curve
             fit_text, fit_z = self.fit(x, z, fit_fn=fit_fn)
             left = 0
             bottom = -0.1
-            self.ax.text(
+            ax.text(
                 left,
                 bottom,
                 fit_text,
                 horizontalalignment="left",
                 verticalalignment="top",
-                transform=self.ax.transAxes,
+                transform=ax.transAxes,
             )
-            self.plot_line(x, fit_z, self.ax, label="fit", color=color)
+
+            if self.plot_setup["zlog"]:
+                ax.set_yscale("log")
+
+            self.plot_line(x, fit_z, ax, label="fit", color=color)
 
         if self.plot_setup["zlimits"]:
-            self.ax.set_ylim(self.plot_setup["zlimits"])
+            ax.set_ylim(self.plot_setup["zlimits"])
 
     def plot_errorbar(self, x, z, axis, zerr, label, color="b"):
         axis.errorbar(

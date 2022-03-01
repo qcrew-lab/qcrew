8a104adb7d00604b9b55c2c9dc60c6ee1a604e99
Atharv Joshi <general.qcrew@gmail.com>
2022-03-01 10:26:35 +0800

    data saving of threshold calc

--- qcrew/measure/RO_calibration/threshold_calculation.py
+++ qcrew/measure/RO_calibration/threshold_calculation.py
@@ -1,6 +1,7 @@
 """ Readout integration weights training for single-shot readout """
 
 from qcrew.control.instruments.meta.readout_trainer import ReadoutTrainer
+from qcrew.helpers.datasaver import DataSaver, initialise_database
 from qcrew.control import Stagehand
 
 if __name__ == "__main__":
@@ -9,12 +10,27 @@ if __name__ == "__main__":
         qm = stage.QM
 
         params = {
-            "reps": 100000,
-            "wait_time": 600000,  # ns
+            "reps": 20000,
+            "wait_time": 400000,  # ns
             "qubit_pi_pulse": "pi",  # pulse to excite qubit
         }
 
-        ro_trainer = ReadoutTrainer(rr, qubit, qm, params)
-        ro_trainer.calculate_threshold()
+        ro_trainer = ReadoutTrainer(rr, qubit, qm, **params)
+        threshold, data = ro_trainer.calculate_threshold()
 
-        ## Make sure to run this script every time the readout pulse is changed!!
+        ## Save data
+        db = initialise_database(
+            exp_name="threshold_calculation",
+            sample_name=stage.sample_name,
+            project_name=stage.project_name,
+            path=stage.datapath,
+        )
+        with DataSaver(db) as datasaver:
+
+            # add metadata dictionary
+            datasaver.add_metadata(ro_trainer.parameters)
+
+            for mode in ro_trainer.modes:
+                datasaver.add_metadata(mode.parameters)
+
+            datasaver.add_multiple_results(data, save=data.keys(), group="data")

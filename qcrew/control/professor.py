45c8c31c80b2cda737002e76141fe4dff540eba3
Atharv Joshi <general.qcrew@gmail.com>
2022-03-10 15:01:43 +0800

    adapted professor to run experiments w/o stage

--- qcrew/control/professor.py
+++ qcrew/control/professor.py
@@ -15,6 +15,78 @@ import matplotlib.pyplot as plt
 from IPython import display
 
 
+def run_with_stage(experiment, stage):
+    """use this method if you don't want the professor to open the stage through the stagehand, but you need to pass in the stage to this method (which you should open in your experiment class __name__ == "__main__" block)"""
+
+    qua_program = experiment.QUA_sequence()
+    qm = stage.QM
+    qm_job = qm.execute(qua_program)
+
+    #########################        INVOKE HELPERS        #########################
+
+    fetcher = QMResultFetcher(handle=qm_job.result_handles)
+    stderr = (None, None, None)  # to hold running (stderr, mean, variance * (n-1))
+
+    db = initialise_database(
+        exp_name=experiment.name,
+        sample_name=stage.sample_name,
+        project_name=stage.project_name,
+        path=stage.datapath,
+    )  # db.filename
+
+    # Update experiment with the filename of the data
+    # Filename will be sent to the plotter as title if no other title is set.
+    experiment.filename = db.filename
+
+    plotter = Plotter(experiment.plot_setup)
+
+    ##################        LIVE POST-PROCESSING LOOP        ####################
+
+    with DataSaver(db) as datasaver:
+
+        ##############        SAVE MEASUREMENT RUN METADATA       ##################
+
+        datasaver.add_metadata(experiment.parameters)
+        for mode in stage.modes:
+            datasaver.add_metadata(mode.parameters)
+
+        while fetcher.is_fetching:
+
+            ###############            FETCH PARTIAL RESULTS         ###############
+
+            partial_results = fetcher.fetch()
+            num_results = fetcher.count
+            if not partial_results:  # empty dict means no new results available
+                continue
+
+            ################            LIVE SAVE RESULTS         ##################
+
+            datasaver.update_multiple_results(
+                partial_results, save=experiment.live_saving_tags, group="data"
+            )
+
+            #######            CALCULATE RUNNING MEAN STANDARD ERROR         #######
+
+            stderr = experiment.estimate_sd(stats, partial_results, num_results, stderr)
+
+            #############            LIVE PLOT AVAILABLE RESULTS         ###########
+
+            data_dict = experiment.plot_results(
+                plotter, partial_results, num_results, stderr
+            )
+            time.sleep(
+                experiment.fetch_period
+            )  # prevent over-fetching, over-saving, ultra-fast plotting
+
+        ##################         SAVE REMAINING DATA         #####################
+
+        datasaver.add_multiple_results(data_dict, save=data_dict.keys(), group="data")
+
+    ##########################          fin           #############################
+
+    print(qm_job.execution_report())
+
+
 def run(experiment: Experiment) -> None:
     """ """
 
@@ -35,74 +107,4 @@ def run(experiment: Experiment) -> None:
 
         #########################        RUN EXPERIMENT        #########################
 
-        qua_program = experiment.QUA_sequence()
-        qm = stage.QM
-        qm_job = qm.execute(qua_program)
-
-        #########################        INVOKE HELPERS        #########################
-
-        fetcher = QMResultFetcher(handle=qm_job.result_handles)
-        stderr = (None, None, None)  # to hold running (stderr, mean, variance * (n-1))
-
-        db = initialise_database(
-            exp_name=experiment.name,
-            sample_name=stage.sample_name,
-            project_name=stage.project_name,
-            path=stage.datapath,
-        )  # db.filename
-
-        # Update experiment with the filename of the data
-        # Filename will be sent to the plotter as title if no other title is set.
-        experiment.filename = db.filename
-
-        plotter = Plotter(experiment.plot_setup)
-
-        ##################        LIVE POST-PROCESSING LOOP        ####################
-
-        with DataSaver(db) as datasaver:
-
-            ##############        SAVE MEASUREMENT RUN METADATA       ##################
-
-            datasaver.add_metadata(experiment.parameters)
-            for mode in stage.modes:
-                datasaver.add_metadata(mode.parameters)
-
-            while fetcher.is_fetching:
-
-                ###############            FETCH PARTIAL RESULTS         ###############
-
-                partial_results = fetcher.fetch()
-                num_results = fetcher.count
-                if not partial_results:  # empty dict means no new results available
-                    continue
-
-                ################            LIVE SAVE RESULTS         ##################
-
-                datasaver.update_multiple_results(
-                    partial_results, save=experiment.live_saving_tags, group="data"
-                )
-
-                #######            CALCULATE RUNNING MEAN STANDARD ERROR         #######
-
-                stderr = experiment.estimate_sd(
-                    stats, partial_results, num_results, stderr
-                )
-
-                #############            LIVE PLOT AVAILABLE RESULTS         ###########
-
-                final_save_dict = experiment.plot_results(
-                    plotter, partial_results, num_results, stderr
-                )
-                time.sleep(
-                    experiment.fetch_period
-                )  # prevent over-fetching, over-saving, ultra-fast plotting
-
-            ##################         SAVE REMAINING DATA         #####################
-
-            datasaver.add_multiple_results(
-                final_save_dict, save=final_save_dict.keys(), group="data"
-            )
-
-        ##########################          fin           #############################
-
-        print(qm_job.execution_report())
+        run_with_stage(experiment, stage)

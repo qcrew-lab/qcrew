077f0aed9c72be5e12f39753ca75ed63a1663c5f
Atharv Joshi <general.qcrew@gmail.com>
2022-03-10 15:03:11 +0800

    script for retrieving sweep data from hdf5 files and plotting

--- qcrew/measure/SQUID_experiments/plot_spectroscopy_vs_current.py
+++ qcrew/measure/SQUID_experiments/plot_spectroscopy_vs_current.py
@@ -3,24 +3,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 from os import listdir
 
-# filepath = (
-#    "C:/Users/qcrew/Desktop/qcrew/data/cc2_transmon/20220204/143302_cc2_qubit_spec.h5"
-# )
+
+def read_results(filepath):
+    data = h5py.File(filepath, "r")["data"]
+    freqs_total = np.array([])  # total list of frequencies LO+IF
+    spec_per_current = {}  # one spectroscopy data per current bias value
+    for experiment in data.keys():
+        if experiment == "exp_99":
+            continue
+        # Each experiment has one qubit_lo and current bias associated with it
+        current = float(np.array(data[experiment]["current"]))
+        qubit_lo = float(np.array(data[experiment]["qubit_LO"]))
+        z_avg = np.array(data[experiment]["Z_AVG"])
+        freqs = np.array(data[experiment]["freqs"])  # spectroscopy frequency lo+if
+        if current not in spec_per_current.keys():
+            spec_per_current[current] = {"Z_AVG": tuple(), "freqs": tuple()}
+        spec_per_current[current]["Z_AVG"] += (z_avg,)
+        spec_per_current[current]["freqs"] += (freqs,)
+
+    # Get freqs_total
+
+    for current in spec_per_current.keys():
+        freqs = np.concatenate(spec_per_current[current]["freqs"])
+        if len(freqs) > len(freqs_total):
+            # update freqs_total
+            freqs_total = freqs
+
+    # build z_matrix
+    z_matrix = []
+    currents_total = []
+    for current in spec_per_current.keys():
+        freqs = np.concatenate(spec_per_current[current]["freqs"])
+        if len(freqs) == len(freqs_total):
+            z_avg = np.concatenate(spec_per_current[current]["Z_AVG"])
+            z_matrix.append(z_avg)
+            currents_total.append(current)
+        else:
+            # spectroscopy was unfinished; disregard data for this current bias
+            print(
+                "Spec for current value of %.3fmA was not concluded. Discarding data."
+                % (current * 1e3)
+            )
+            continue
+
+    return freqs_total, currents_total, np.array(z_matrix)
+
 
 data_folders = [
-    "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220304/",
-    "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220305/",
-    "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220306/",
-    "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220307/",
+    "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220309/",
 ]
 file_list = []
 for i in range(len(data_folders)):
-    file_list += ["%d_" % (i + 4) + s for s in listdir(data_folders[i])]
+    file_list += ["%d_" % (i + 9) + s for s in listdir(data_folders[i])]
 
 # get only relevant experiments
-first_file = "4_155832_cc2_rr_spec.h5"
+first_file = "9_154827_cc2_rr_spec.h5"
 first_index = file_list.index(first_file)
-last_file = "7_073441_cc2_qubit_spec.h5"
+last_file = "9_163312_cc2_qubit_spec.h5"
 last_index = file_list.index(last_file)
 file_list = file_list[first_index : last_index + 1]
 
@@ -38,7 +77,7 @@ current_sweep = np.arange(current_start, current_stop, current_step)
 qubit_lo_start = 5.42733e9
 qubit_lo_stop = 4e9
 qubit_lo_step = -200e6
-qubit_lo_sweep = list(np.arange(qubit_lo_start, qubit_lo_stop, qubit_lo_step))
+qubit_lo_sweep = np.arange(qubit_lo_start, qubit_lo_stop, qubit_lo_step)
 
 sweep_params = []
 for current in current_sweep:
@@ -47,6 +86,7 @@ for current in current_sweep:
 
 i = 0
 Z_matrix = []
+freqs_list = []
 for current in current_sweep:
 
     Z_row = []
@@ -55,7 +95,7 @@ for current in current_sweep:
         if i < len(file_list):
             print(sweep_params[i][1])
             file_name = file_list[i]
-            file_name = data_folders[int(file_name.split("_")[0]) - 4] + "_".join(
+            file_name = data_folders[int(file_name.split("_")[0]) - 9] + "_".join(
                 file_name.split("_")[1:]
             )
             file = h5py.File(file_name, "r")
@@ -80,31 +120,19 @@ for current in current_sweep:
         freqs_list = []
         Z_matrix.append(np.array(Z_row))
 
-Z_matrix = np.array(Z_matrix)
-print(Z_matrix)
-print(Z_matrix.shape)
-np.isnan(Z_matrix)
+z_matrix_2 = np.array(Z_matrix)
+currents_2 = current_sweep[:40]
+
+
+filepath_list = "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220309/190957_cc2_qubit_spec_current_sweep.h5"
+freqs, currents_1, z_matrix_1 = read_results(filepath_list)
+
+filepath_list = "C:/Users/qcrew/Desktop/qcrew/data/cc2_deltaSQUID/20220310/095520_cc2_qubit_spec_current_sweep.h5"
+
+freqs, currents_2, z_matrix_2 = read_results(filepath_list)
+currents = np.concatenate((np.array(currents_1), currents_2), axis=0)
+z_matrix = np.concatenate((z_matrix_1, z_matrix_2), axis=0)
+
 fig, ax = plt.subplots()
-X, Y = np.meshgrid(*(current_sweep[:16], freqs_list))
-ax.pcolormesh(X, Y, Z_matrix.T)
-plt.show()
-# print(listdir(data_folder_1))
-
-
-"""
-currents = ["-0.10mA", "-0.05mA", "0.00mA", "0.05mA", "0.10mA", "0.15mA"]
-
-plt.figure(figsize=(12, 8))
-for i, current in enumerate(currents):
-    file = h5py.File(filepaths[i], "r")
-    data = file["data"]
-    z_avg = data["Z_AVG"]
-    x = data["x"]
-    plt.plot(np.array(x)[110:], np.array(z_avg)[110:], label=current)
-
-plt.ylabel("Signal level (a.u.)")
-plt.xlabel("IF frequency of qubit pulse (Hz)")
-plt.title("Qubit spectroscopy as a function of the current bias")
-plt.legend()
-plt.show()
-"""
+X, Y = np.meshgrid(*(currents, freqs))
+ax.pcolormesh(X, Y, z_matrix.T, cmap="Blues")

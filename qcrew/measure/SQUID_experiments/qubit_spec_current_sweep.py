e419a3a5360c1b628d513c6d1063627061a168b3
Atharv Joshi <general.qcrew@gmail.com>
2022-03-10 15:03:32 +0800

    experiment for qubit spec as a function of current (WIP)

--- qcrew/measure/SQUID_experiments/qubit_spec_current_sweep.py
+++ qcrew/measure/SQUID_experiments/qubit_spec_current_sweep.py
@@ -6,18 +6,25 @@ This class serves as a QUA script generator with user-defined parameters.
 
 from qcrew.control import professor as prof
 from qcrew.control import Stagehand
+from qcrew.control.instruments.qm import QMResultFetcher
+
+from qcrew.helpers.datasaver import DataSaver, initialise_database
+
+from qcrew.analyze.plotter import Plotter
+from qcrew.analyze import stats
 
 from qcrew.measure.qubit_experiments_GE.qubit_spec import QubitSpectroscopy
 from qcrew.measure.resonator_characterization.rr_spec import RRSpectroscopy
 
 import numpy as np
 import h5py
+import time
 
 # -------------------------------- Execution -----------------------------------
 
 if __name__ == "__main__":
 
-    current_start = 10e-3
+    current_start = 4.5e-3
     current_stop = -10.1e-3
     current_step = -0.5e-3
     current_sweep = np.arange(current_start, current_stop, current_step)
@@ -33,108 +40,173 @@ if __name__ == "__main__":
     lo_list = []
     rr_if_list = []
 
-    for current in current_sweep:
+    # Initialize database for compiling whole experimental data
+    with Stagehand() as stage:
 
-        # Change value of current source
-        with Stagehand() as stage:
-            yoko = stage.YOKO
-            yoko.source = "current"
-            yoko.level = current  # set output to nominal value
-            yoko.state = True
+        db = initialise_database(
+            exp_name="qubit_spec_current_sweep",
+            sample_name=stage.sample_name,
+            project_name=stage.project_name,
+            path=stage.datapath,
+        )
 
-        # Find resonator resonant frequency
-        ## Do RR spectroscopy
-        x_start = -56e6
-        x_stop = -46e6
-        x_step = 0.05e6
+        with DataSaver(db) as datasaver:
+            experiment_no = 1
+            for current in current_sweep:
 
-        rr_spec_parameters = {
-            "modes": ["RR"],
-            "reps": 10,
-            "wait_time": 10000,
-            "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
-            "fit_fn": None,
-        }
-
-        rr_spec_plot_parameters = {
-            "xlabel": "Resonator pulse frequency (Hz)",
-        }
-
-        rr_spec = RRSpectroscopy(**rr_spec_parameters)
-        rr_spec.setup_plot(**rr_spec_plot_parameters)
-
-        prof.run(rr_spec)
-
-        ## Get frequency of MAXIMUM transmission (transmission measurement)
-        filepath = rr_spec.filename
-        data = h5py.File(filepath, "r")["data"]
-        z_avg = np.array(data["Z_AVG"])
-        frequencies = np.array(data["x"])
-        rr_if = frequencies[np.argmax(z_avg)]
-        ## Change RR IF accordingly
-
-        with Stagehand() as stage:
-            rr = stage.RR
-            rr.int_freq = float(rr_if)
-
-        # proceed with qubit spectroscopy
-        for qubit_lo in qubit_lo_sweep:
-
-            with Stagehand() as stage:
-                qubit = stage.QUBIT
-                qubit.lo_freq = qubit_lo
-
-            x_start = -200e6
-            x_stop = 0e6
-            x_step = 0.5e6
-
-            qubit_spec_parameters = {
-                "modes": ["QUBIT", "RR"],
-                "reps": 10,
-                "wait_time": 100000,
-                "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
-                "qubit_op": "constant_pulse",
-                "fit_fn": None,
-            }
-
-            qubit_spec_plot_parameters = {
-                "xlabel": "Qubit pulse frequency (Hz) (LO = %.3f GHz)"
-                % (qubit_lo / 1e9),
-            }
-
-            qubit_spec = QubitSpectroscopy(**qubit_spec_parameters)
-            qubit_spec.setup_plot(**qubit_spec_plot_parameters)
-
-            prof.run(qubit_spec)
-
-            filename_list.append(qubit_spec.filename)
-            current_list.append(current)
-            lo_list.append(qubit_lo)
-            rr_if_list.append(rr_if)
-
-            with Stagehand() as stage:
+                # Change value of current source
                 yoko = stage.YOKO
-                yoko.state = False
+                yoko.source = "current"
+                yoko.level = current  # set output to nominal value
+                yoko.state = True
 
-            # Retrieve the saved data
-            file = h5py.File(qubit_spec.filename, "r")
-            z_avg = np.array(data["Z_AVG"])
+                # Find resonator resonant frequency
+                ## Do RR spectroscopy
+                x_start = -56e6
+                x_stop = -46e6
+                x_step = 0.05e6
 
-            ## Correct floor of z_avg
-            z_avg -= np.average(z_avg)
-            z_avg = list(z_avg)
-            if current_step < 0:
-                z_avg.reverse()
+                rr_spec_parameters = {
+                    "modes": [stage.RR],
+                    "reps": 2000,
+                    "wait_time": 10000,
+                    "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
+                    "fit_fn": None,
+                    "fetch_period": 2,
+                }
 
-            ## Get frequencies
-            freqs = np.array(data["x"]) + qubit_lo
-            freqs = list(freqs)
-            if current_step < 0:
-                freqs.reverse()
+                rr_spec_plot_parameters = {
+                    "xlabel": "Resonator pulse frequency (Hz)",
+                }
 
-    for i, filename in enumerate(filename_list):
-        print("Experiment number %d: " % i)
-        print("    qubit lo = %.8f GHz" % (lo_list[i] / 1e9))
-        print("    current  = %.2f mA" % (current_list[i] / 1e-3))
-        print("    rr IF    = %.3f MHz" % (rr_if_list[i] / 1e6))
-        print(filename)
+                rr_spec = RRSpectroscopy(**rr_spec_parameters)
+                rr_spec.setup_plot(**rr_spec_plot_parameters)
+                plotter = Plotter(rr_spec.plot_setup)
+
+                # prof.run_with_stage(rr_spec, stage)
+                stderr = (None, None, None)
+                qua_program = rr_spec.QUA_sequence()
+                qm = stage.QM
+                qm_job = qm.execute(qua_program)
+                fetcher = QMResultFetcher(handle=qm_job.result_handles)
+
+                while fetcher.is_fetching:
+                    # Fetch partial results
+                    partial_results = fetcher.fetch()
+                    num_results = fetcher.count
+                    if not partial_results:
+                        # empty dict means no new results available
+                        continue
+
+                    # Calculate standard error
+                    stderr = rr_spec.estimate_sd(
+                        stats, partial_results, num_results, stderr
+                    )
+
+                    # post-process and live plot available results
+                    rr_spec_data = rr_spec.plot_results(
+                        plotter, partial_results, num_results, stderr
+                    )
+                    # prevent over-fetching, over-saving, ultra-fast plotting
+                    time.sleep(rr_spec.fetch_period)
+
+                ## Get frequency of MAXIMUM transmission (transmission measurement)
+                z_avg = np.array(rr_spec_data["Z_AVG"])
+                frequencies = np.array(rr_spec_data["x"])
+                rr_if = frequencies[np.argmax(z_avg)]
+
+                stage.RR.int_freq = float(rr_if)  # update RR IF
+
+                # proceed with qubit spectroscopy
+                for qubit_lo in qubit_lo_sweep:
+
+                    ## Change qubit LO
+                    stage.QUBIT.lo_freq = qubit_lo
+
+                    x_start = -200e6
+                    x_stop = 0e6
+                    x_step = 0.5e6
+
+                    qubit_spec_parameters = {
+                        "modes": [stage.QUBIT, stage.RR],
+                        "reps": 4000,
+                        "wait_time": 100000,
+                        "x_sweep": (
+                            int(x_start),
+                            int(x_stop + x_step / 2),
+                            int(x_step),
+                        ),
+                        "qubit_op": "constant_pulse",
+                        "fit_fn": None,
+                        "fetch_period": 2,
+                    }
+
+                    qubit_spec_plot_parameters = {
+                        "xlabel": "Qubit pulse frequency (Hz) (LO = %.3f GHz)"
+                        % (qubit_lo / 1e9),
+                    }
+
+                    qubit_spec = QubitSpectroscopy(**qubit_spec_parameters)
+                    qubit_spec.setup_plot(**qubit_spec_plot_parameters)
+                    plotter = Plotter(qubit_spec.plot_setup)
+
+                    ##### run qubit spectroscopy #####
+                    # prof.run_with_stage(qubit_spec, stage)
+                    stderr = (None, None, None)
+                    qua_program = qubit_spec.QUA_sequence()
+                    qm = stage.QM
+                    qm_job = qm.execute(qua_program)
+                    fetcher = QMResultFetcher(handle=qm_job.result_handles)
+
+                    while fetcher.is_fetching:
+                        # Fetch partial results
+                        partial_results = fetcher.fetch()
+                        num_results = fetcher.count
+                        if not partial_results:
+                            # empty dict means no new results available
+                            continue
+
+                        # Calculate standard error
+                        stderr = qubit_spec.estimate_sd(
+                            stats, partial_results, num_results, stderr
+                        )
+
+                        # post-process and live plot available results
+                        qubit_spec_data = qubit_spec.plot_results(
+                            plotter, partial_results, num_results, stderr
+                        )
+                        # prevent over-fetching, over-saving, ultra-fast plotting
+                        time.sleep(qubit_spec.fetch_period)
+
+                    ## Correct floor of z_avg
+                    z_avg = np.array(qubit_spec_data["Z_AVG"])
+                    z_avg -= np.average(z_avg)
+                    z_avg = list(z_avg)
+                    if current_step < 0:
+                        z_avg.reverse()
+
+                    ## Get frequencies
+                    freqs = np.array(qubit_spec_data["x"]) + qubit_lo
+                    freqs = list(freqs)
+                    if current_step < 0:
+                        freqs.reverse()
+
+                    # save data
+                    exp_tag = "exp_%d" % experiment_no
+                    current_sweep_data = {
+                        "current": float(current),
+                        "qubit_LO": float(qubit_lo),
+                        "Z_AVG": np.array(z_avg),
+                        "freqs": np.array(freqs),
+                    }
+
+                    datasaver.add_multiple_results(
+                        current_sweep_data,
+                        save=current_sweep_data.keys(),
+                        group="data/" + exp_tag,
+                    )
+
+                    experiment_no += 1
+
+            yoko = stage.YOKO
+            yoko.state = False

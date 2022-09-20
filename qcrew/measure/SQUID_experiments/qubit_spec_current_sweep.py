78a1622f7ad0ed4903ae6446cef818c8d9f7cf7a
qcrew-lab <general.qcrew@gmail.com>
2022-09-20 10:21:48 +0800

    added mixer tuning

--- qcrew/measure/SQUID_experiments/qubit_spec_current_sweep.py
+++ qcrew/measure/SQUID_experiments/qubit_spec_current_sweep.py
@@ -20,18 +20,23 @@ import numpy as np
 import h5py
 import time
 
+from qcrew.control.instruments.meta.mixer_tuner import MixerTuner
+from qcrew.control import Stagehand
+from qcrew.measure.resonator_characterization.rr_amp_calibration import RRAmpCalibration
+
+
 # -------------------------------- Execution -----------------------------------
 
 if __name__ == "__main__":
 
-    current_start = 10e-3
-    current_stop = -10e-3
-    current_step = -0.5e-3
+    current_start = -70e-3
+    current_stop = 35e-3
+    current_step = 5e-3
     current_sweep = np.arange(current_start, current_stop, current_step)
 
-    qubit_lo_start = 5.4084e9
-    qubit_lo_stop = 4e9
-    qubit_lo_step = -200e6
+    qubit_lo_start = 4e9
+    qubit_lo_stop = 5.801e9
+    qubit_lo_step = 200e6
     qubit_lo_sweep = np.arange(qubit_lo_start, qubit_lo_stop, qubit_lo_step)
 
     # Store information in lists to print later
@@ -63,13 +68,13 @@ if __name__ == "__main__":
                 # Find resonator resonant frequency
                 ## Do RR spectroscopy
                 x_start = -60e6
-                x_stop = -46e6
+                x_stop = -40e6
                 x_step = 0.05e6
 
                 rr_spec_parameters = {
                     "modes": [stage.RR],
                     "reps": 2000,
-                    "wait_time": 10000,
+                    "wait_time": 5000,
                     "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
                     "fit_fn": None,
                     "fetch_period": 2,
@@ -115,15 +120,69 @@ if __name__ == "__main__":
                 ## Get frequency of MAXIMUM transmission (transmission measurement)
                 # rr_if = frequencies[np.argmax(z_avg)]
                 ## Get frequency of MINIMUM transmission (reflection measurement)
-                rr_if = frequencies[np.argmin(z_avg)]
-
-                stage.RR.int_freq = float(rr_if)  # update RR IF
-
+                rr_if = frequencies[np.argmin(z_avg)] 
+                print("a")
+                stage.RR.int_freq = float(rr_if)  + 0.2e6 # update RR IF
+                print("b")
                 # proceed with qubit spectroscopy
                 for qubit_lo in qubit_lo_sweep:
 
                     ## Change qubit LO
                     stage.QUBIT.lo_freq = qubit_lo
+
+                    ## tune the mixer
+                    qm = stage.QM
+                    mixer_tuner = MixerTuner(stage.SA, qm)
+
+                    # this is the mode whose mixer's LO or SB leakage you are tuning
+                    mode = stage.QUBIT
+
+                    # minimize LO leakage
+
+                    # use brute force (BF) minimizer
+                    bf_params_lo = {
+                        # range of DC offsets you want to sweep to tune LO
+                        "offset_range": (-0.2, 0.2),  # (min = -0.5, max = 0.5)
+                        # number of DC offset sweep points in the given range i.e. decide step size
+                        "num_points": 21,
+                        # number of iterations of the minimization you want to run
+                        "num_iterations": 5,
+                        # after each iteration, the sweep range will be reduced by this factor
+                        "range_divider": 2,
+                        # if you want the full minimization traceback, set this to True
+                        "verbose": True,
+                        # if you want a plot that shows minimization summary, set this to True
+                        "plot": False,
+                    }
+
+                    #mixer_tuner.tune_lo(mode=mode, method="BF", **bf_params_lo)
+
+                    # user Nelder-Mead (NM) minimizer
+                    mixer_tuner.tune_lo(mode=mode, method="NM")
+
+                    # minimize SB leakage
+
+                    # use brute force (BF) minimizer
+                    bf_params_sb = {
+                        # range of DC offsets you want to sweep to tune LO
+                        "offset_range": (-0.2, 0.2),  # (min = -0.5, max = 0.5)
+                        # number of DC offset sweep points in the given range i.e. decide step size
+                        "num_points": 21,
+                        # number of iterations of the minimization you want to run
+                        "num_iterations": 5,
+                        # after each iteration, the sweep range will be reduced by this factor
+                        "range_divider": 2,
+                        # if you want the full minimization traceback, set this to True
+                        "verbose": True,
+                        # if you want a plot that shows minimization summary, set this to True
+                        "plot": False,
+                    }
+
+                    #mixer_tuner.tune_sb(mode=mode, method="BF", **bf_params_lo)
+
+                    # user Nelder-Mead (NM) minimizer
+                    mixer_tuner.tune_sb(mode=mode, method="NM")
+
 
                     x_start = -200e6
                     x_stop = 0e6
@@ -131,8 +190,8 @@ if __name__ == "__main__":
 
                     qubit_spec_parameters = {
                         "modes": [stage.QUBIT, stage.RR],
-                        "reps": 500,
-                        "wait_time": 50000,
+                        "reps": 6000,
+                        "wait_time": 5000,
                         "x_sweep": (
                             int(x_start),
                             int(x_stop + x_step / 2),

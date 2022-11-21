69d0832645c4949931153713669a9cc02336bacc
Atharv Joshi <general.qcrew@gmail.com>
2022-11-21 10:28:58 +0800

    update run params

--- qcrew/measure/vna/vnasweep.py
+++ qcrew/measure/vna/vnasweep.py
@@ -101,8 +101,8 @@ if __name__ == "__main__":
         vna.connect()
 
         # the routine below can handle multiple measurement runs at once
-        fcenterlist = [6.52316e9, 6.71602e9, 6.92891e9, 7.15804e9]
-        fspanlist = [500e3, 1e6, 500e3, 1e6]
+        fcenterlist = [6.563419e9] #[6.563419e9, 6.792209e9, 6.874604e9, 7.057255e9]
+        fspanlist = [500e3] #[500e3, 1e6, 250e3, 1e6]
 
         num_runs = len(fcenterlist)
         for idx in range(num_runs):
@@ -119,7 +119,7 @@ if __name__ == "__main__":
                 # IF bandwidth (Hz), [1, 500000]
                 "bandwidth": 1e2,
                 # number of frequency sweep points, [2, 200001]
-                "sweep_points": 2001,
+                "sweep_points": 2501,
                 # delay (s) between successive sweep points, [0.0, 100.0]
                 "sweep_delay": 1e-3,
                 # trace data to be displayed and acquired, max traces = 16
@@ -136,7 +136,7 @@ if __name__ == "__main__":
             # these parameters are looped over during the measurement
             measurement_parameters = {
                 # Number of sweep averages, must be an integer > 0
-                "repetitions": 25,
+                "repetitions": 150,
                 # Input powers at (<port1>, <port2>) (dBm), range [-30.0, 15.0]
                 # <portX> (X=1,2) can be a set {a, b,...}, tuple (st, stop, step), or constant x
                 # use set for discrete sweep points a, b, ...
@@ -146,10 +146,10 @@ if __name__ == "__main__":
                 # eg 1: powers = ((-30, 15, 5), 0) will sweep port 1 power from -30dBm to 15dBm inclusive in steps of 5dBm with port 2 power remaining constant at 0 dBm
                 # eg 2: powers = ({-15, 0, 15}, {-5, 0}) will result in sweep points (-15, -5), (-15, 0), (0, -5), (0, 0), (15, -5), (15, 0)
                 # eg 3: powers = (0, 0) will set both port powers to 0, no power sweep happens
-                "powers": (0, 0),
+                "powers": (10, 0),
                 # total physical attenuation added to VNA ports, if any
                 # (port_1_attenuation, port_2_attenuation) in dB
-                "attenuation": (40.0, 0),
+                "attenuation": (80.0, 0),
             }
 
             # create measurement instance with instruments and measurement_parameters

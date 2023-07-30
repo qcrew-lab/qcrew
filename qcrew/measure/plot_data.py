027f3e16f5d88549ef1afbcf568e51484ef8ba6e
qcrew-lab <general.qcrew@gmail.com>
2023-07-30 09:55:57 +0800

    revised

--- qcrew/measure/plot_data.py
+++ qcrew/measure/plot_data.py
@@ -3,21 +3,37 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-filepath_list = [
-    "C:/Users/qcrew/Desktop/qcrew/data/somerset/20230503/173031_somerset_rr_spec.h5",
-]
+#filepath = "C:/Users/qcrew/Desktop/qcrew/data/somerset/20230711/150526_cut.h5"
+filepath = "C:/Users/qcrew/Desktop/qcrew/data/somerset/20230714/171313_somerset_number_split_spec.h5"
+
 plt.figure(figsize=(8, 5))
-for i, filepath in enumerate(filepath_list):
-    file = h5py.File(filepath, "r")
-    data = file["data"]
-    z_avg = data["Z_AVG"]  # data["PHASE"]
-    x = data["x"]
-    f = x[:,0]
-    for trace in (np.array(z_avg).T): #[:3]
-        trace = trace - np.average(trace)
-        print(f[np.argmin(trace)])
-        plt.plot(np.array(f), np.array(trace))
-    plt.ylabel("Z_AVG")  # phase
-    plt.xlabel("IF frequency (Hz)")  # . Qubit LO = 1.4 GHz")
-    plt.title(filepath_list)
-    plt.show()
+file = h5py.File(filepath, "r")
+data = file["data"]
+print(data.keys())
+state = data["I_AVG"]  # data["PHASE"] Z_AVG
+x = data["x"]
+print(len(state))
+#print(np.where(x == -95000000))
+#print(x[150])
+
+plt.plot(np.array(x), np.array(state))
+plt.show()
+
+# filepath_list = [
+#     "C:/Users/qcrew/Desktop/qcrew/data/somerset/20230503/173031_somerset_rr_spec.h5",
+# ]
+# plt.figure(figsize=(8, 5))
+# for i, filepath in enumerate(filepath_list):
+#     file = h5py.File(filepath, "r")
+#     data = file["data"]
+#     z_avg = data["Z_AVG"]  # data["PHASE"]
+#     x = data["x"]
+#     f = x[:,0]
+#     for trace in (np.array(z_avg).T): #[:3]
+#         trace = trace - np.average(trace)
+#         print(f[np.argmin(trace)])
+#         plt.plot(np.array(f), np.array(trace))
+#     plt.ylabel("Z_AVG")  # phase
+#     plt.xlabel("IF frequency (Hz)")  # . Qubit LO = 1.4 GHz")
+#     plt.title(filepath_list)
+#     plt.show()

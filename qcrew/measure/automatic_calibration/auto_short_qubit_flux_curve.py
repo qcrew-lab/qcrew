c5a415b2c5d57bf549432ef57b1e5c6d6b0a32b8
qcrew-lab <general.qcrew@gmail.com>
2023-11-16 10:38:48 +0800

    add auto cal

---
+++ qcrew/measure/automatic_calibration/auto_short_qubit_flux_curve.py
@@ -0,0 +1,109 @@
+"""
+A python class describing a qubit spectroscopy using QM.
+This class serves as a QUA script generator with user-defined parameters.
+"""
+
+from typing import ClassVar
+
+from qcrew.control import Stagehand
+from qcrew.control import professor as prof
+from qcrew.measure.experiment import Experiment
+from qcrew.analyze import fit
+from qm import qua
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+from datetime import datetime
+
+# ---------------------------------- Class -------------------------------------
+
+
+class QubitSpectroscopy(Experiment):
+    name = "qubit_spec"
+    _parameters: ClassVar[set[str]] = Experiment._parameters | {
+        "qubit_op",  # operation used for exciting the qubit
+        "fit_fn",  # fit function
+    }
+
+    def __init__(self, qubit_op, fit_fn=None, **other_params):
+        self.qubit_op = qubit_op
+        self.fit_fn = fit_fn
+        super().__init__(**other_params)  # Passes other parameters to parent
+
+    def QUA_play_pulse_sequence(self):
+        """
+        Defines pulse sequence to be played inside the experiment loop
+        """
+        qubit, rr, cav, flux = self.modes  # get the modes
+
+        qua.update_frequency(qubit.name, self.x)  # update resonator pulse frequency
+        qubit.play(self.qubit_op)  # play qubit pulse
+        qua.align(qubit.name, rr.name)  # wait qubit pulse to end
+        rr.measure((self.I, self.Q))  # measure transmitted signal
+        qua.wait(int(self.wait_time // 4), rr.name)  # wait system reset
+
+        if self.single_shot:  # assign state to G or E
+            qua.assign(
+                self.state, qua.Cast.to_fixed(self.I < rr.readout_pulse.threshold)
+            )
+
+        self.QUA_stream_results()  # stream variables (I, 4Q, x, etc)
+        qua.align()
+
+
+# -------------------------------- Execution -----------------------------------
+
+if __name__ == "__main__":
+
+    x_start = -200e6
+    x_stop = -0e6
+    x_step = 0.05e6
+
+    current_start = -30.0e-3
+    current_stop = 0e-3
+    current_step = 2e-3
+    current_sweep = np.arange(current_start, current_stop, current_step)
+
+    parameters = {
+        "modes": ["RR"],
+        "reps": 1000,
+        "wait_time": 20000,
+        "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
+        "fit_fn": "gaussian",
+        "plot_quad": "Z_AVG",
+    }
+
+    plot_parameters = {"skip_plot": False, "plot_err": False}
+    data_tag = "state" if parameters.get("single_shot") else parameters["plot_quad"]
+
+    # Initialize an empty array to store values
+    rr_freq_list = np.array([])
+    current_list = np.array([])
+
+    path = "C:\\Users\\qcrew\\Desktop\\qcrew\\qcrew\\config\\rrfreq_vs_current\\"
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S_")
+    rr_freq_filename = path + current_datetime + "rr_current.npz"
+
+    for current in current_sweep:
+
+        with Stagehand() as stage:
+            yoko = stage.YOKO
+            rr = stage.RR
+            yoko.ramp(current, yoko.level, np.abs(0.01e-3))
+            rr_LO = rr.lo_freq
+
+        experiment = RRSpectroscopy(**parameters)
+        experiment.setup_plot(**plot_parameters)
+        prof.run(experiment)
+
+        with h5py.File(experiment._filename, "r") as file:
+            data = file["data"]
+            state = np.array(data[data_tag])
+            freqs = np.array(data["x"])
+            params = fit.do_fit("gaussian", freqs, state)
+            rr_freq = rr_LO + float(params["x0"].value)
+
+            print(rr_freq)
+            rr_freq_list = np.append(rr_freq_list, rr_freq)
+            current_list = np.append(current_list, current)
+            np.savez(rr_freq_filename, rr_freq=rr_freq_list, currents=current_list)

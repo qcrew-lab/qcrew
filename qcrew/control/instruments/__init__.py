9ebf877cb846168d737c95da7cdf693f63707bf1
qcrew-lab <general.qcrew@gmail.com>
2023-11-16 10:50:47 +0800

    add anapico

--- qcrew/control/instruments/__init__.py
+++ qcrew/control/instruments/__init__.py
@@ -5,5 +5,6 @@ from qcrew.control.instruments.vaunix.labbrick import LabBrick
 from qcrew.control.instruments.signal_hound.sa124 import Sa124
 from qcrew.control.instruments.anritsu.vna_ms46522b import VNA
 from qcrew.control.instruments.yokogawa.gs200 import Yoko
+from qcrew.control.instruments.anapico.apuasyn20 import Apuasyn
 from qcrew.control.instruments.signalcore.sc5511a import CoreA
 from qcrew.control.instruments.signalcore.sc5503b import CoreB

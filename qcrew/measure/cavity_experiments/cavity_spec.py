2e5009b4b57d29eca7347a2a84cc61eee6a8075f
qcrew-lab <general.qcrew@gmail.com>
2023-12-23 17:17:22 +0800

    exp para

--- qcrew/measure/cavity_experiments/cavity_spec.py
+++ qcrew/measure/cavity_experiments/cavity_spec.py
@@ -36,17 +36,23 @@ class CavitySpectroscopy(Experiment):
         Defines pulse sequence to be played inside the experiment loop
         """
         qubit, cav, rr, flux = self.modes  # get the modes
-        # qubit.play(self.qubit_op, ampx=self.y)
+        
         qua.update_frequency(cav.name, self.x)  # update resonator pulse frequency
+        qubit.play("gaussian_pi_hk", ampx=self.y)
         qua.align()  # align all modes
         cav.play(self.cav_op)  # play displacement to cavity
         qua.align(cav.name, qubit.name)  # align all modes
         qubit.play(self.qubit_op)  # play qubit pulse
         qua.align()  # wait qubit pulse to end
-        # flux.play("constcos80ns_2000ns_E2pF2pG2pH2", ampx=-0.3)
-        # qua.wait(int(220 // 4), rr.name, "QUBIT_EF")
-        qua.play("digital_pulse", "QUBIT_EF")
-        rr.measure((self.I, self.Q), ampx=0)  # measure qubit state
+        if 0:
+            flux.play("constcos20ns_tomo_RO_tomo_new_E2pF2pG2pH2_3", ampx=-0.5685)  # lk
+        if 0:
+            flux.play("constcos80ns_tomo_RO_tomo_E2pF2pG2pH2", ampx=0.1)  # rr
+        if 0:
+            flux.play("constcos80ns_tomo_RO_tomo_E2pF2pG2pH2", ampx=0.0526)  # hk
+        # qua.wait(int(30 // 4), rr.name, "QUBIT_EF")  # ns
+        # qua.play("digital_pulse", "QUBIT_EF")
+        rr.measure((self.I, self.Q))  # measure qubit state
 
         qua.wait(int(self.wait_time // 4), cav.name)  # wait system reset
 
@@ -64,21 +70,21 @@ class CavitySpectroscopy(Experiment):
 if __name__ == "__main__":
 
     x_start = -45e6
-    x_stop = -35e6
-    x_step = 0.25e6
+    x_stop = -32e6
+    x_step = 0.2e6
     parameters = {
         "modes": ["QUBIT", "CAVITY", "RR", "FLUX"],
-        "reps": 10000,
-        "wait_time": 1e6,
+        "reps": 100000,
+        "wait_time": 0.5e6,
         "x_sweep": (int(x_start), int(x_stop + x_step / 2), int(x_step)),
-        # "y_sweep": (0.0, 1.0),
-        "qubit_op": "gaussian_pi_560",
+        "y_sweep": (0.0, 1.0),
+        "qubit_op": "gaussian_pi_hk_320",
         "plot_quad": "I_AVG",
-        "fetch_period": 1,
+        "fetch_period": 10,
         # "single_shot": True,
         # "cav_op": "cohstate_1_long",
-        "cav_op": "gaussian_spectroscopy_pulse",
-        # "fetch_period": 5,
+        "cav_op": "spectroscopy_pulse",
+
     }
 
     plot_parameters = {

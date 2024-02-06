7bc158e9045550738a8fd7c3946574482171939d
qcrew-lab <general.qcrew@gmail.com>
2024-02-06 14:58:00 +0800

    changed some vna plotting

--- qcrew/measure/vna/plot_vna.py
+++ qcrew/measure/vna/plot_vna.py
@@ -2,13 +2,15 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-filepath = "C:/Users/qcrew/Desktop/qcrew/data/djext/20230712/181013_ampfluxsweep_.hdf5"
+filepath = "C:/Users/qcrew/Desktop/qcrew/data/exitB_rr/20240205/201807_vnasweep_5500000000.0_-15pow_1reps.hdf5"
 file = h5py.File(filepath, "r")
 data = file["data"]
 s21_mlog = data["s21_mlog"]
 freqs = data["frequency"]
-s21_mlog_avg = np.mean(s21_mlog, axis=0)
+print(np.array(freqs).shape)
+print(np.array(s21_mlog)[0].shape)
+# s21_mlog_avg = np.mean(s21_mlog, axis=0)
 plt.figure(figsize=(12, 8))
-plt.plot(np.array(freqs), s21_mlog_avg[0])
+plt.plot(np.array(freqs), np.array(s21_mlog)[0])
 plt.legend()
 plt.show()
